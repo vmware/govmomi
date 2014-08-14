5a0e65e53c5e9575aab714b6d35cd609a2d6caf6
Pieter Noordhuis <pnoordhuis@vmware.com>
2014-08-13 18:07:53 -0700

    Import scripts used for code generation

---
+++ gen/gen_from_wsdl.rb
@@ -0,0 +1,76 @@
+# Copyright (c) 2014 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$:.unshift(File.expand_path(File.dirname(__FILE__)))
+
+require "vim_wsdl"
+
+if !File.directory?(ARGV.first)
+  raise "first argument not a directory"
+end
+
+wsdl = WSDL.new(WSDL.read "vim.wsdl")
+wsdl.validate_assumptions!
+wsdl.peek()
+
+ifs = Peek.types.keys.select { |name| Peek.base?(name) }.size()
+puts "%d classes, %d interfaces" % [Peek.types.size(), ifs]
+
+types_go = File.open(File.join(ARGV.first, "types/types.go"), "w")
+io = types_go
+io.print "package types\n\n"
+
+wsdl.
+  types.
+  sort_by { |x| x.name }.
+  uniq { |x| x.name }.
+  select { |x| x.name[0] == x.name[0].upcase }. # Only capitalized methods for now...
+  select { |t| t.is_enum? }.
+  each { |e| e.dump(io); e.dump_init(io) }
+
+wsdl.
+  types.
+  sort_by { |x| x.name }.
+  uniq { |x| x.name }.
+  select { |x| x.name[0] == x.name[0].upcase }. # Only capitalized methods for now...
+  select { |t| !t.is_enum? }.
+  each { |e| e.dump(io); e.dump_init(io) }
+
+if_go = File.open(File.join(ARGV.first, "types/if.go"), "w")
+io = if_go
+io.print "package types\n\n"
+Peek.dump_interfaces(io)
+
+methods_go = File.open(File.join(ARGV.first, "methods/methods.go"), "w")
+io = methods_go
+io.print "package methods\n\n"
+
+wsdl.
+  operations.
+  sort_by { |x| x.name }.
+  select { |x| x.name[0] == x.name[0].upcase }. # Only capitalized methods for now...
+  each { |e| e.dump(io) }
+
+methods_go = File.open(File.join(ARGV.first, "tasks/tasks.go"), "w")
+io = methods_go
+io.print "package tasks\n\n"
+
+wsdl.
+  operations.
+  sort_by { |x| x.name }.
+  select { |x| x.name[0] == x.name[0].upcase }. # Only capitalized methods for now...
+  select { |x| x.name =~ /_Task$/ }.
+  each { |e| e.dump_task(io) }
+
+exit(0)
