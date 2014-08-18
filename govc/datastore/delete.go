02f4008518520f85e3f37e0dbaef5b4231f9182b
Doug MacEachern <dougm@vmware.com>
2014-08-18 12:16:59 -0700

    Add datastore.delete command

---
+++ govc/datastore/delete.go
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2014 VMware, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package datastore
+
+import (
+	"flag"
+
+	"github.com/vmware/govmomi/govc/cli"
+	"github.com/vmware/govmomi/govc/flags"
+)
+
+type delete struct {
+	*flags.ClientFlag
+	*flags.DatastorePath
+}
+
+func init() {
+	cli.Register(&delete{})
+}
+
+func (c *delete) Register(f *flag.FlagSet) {
+}
+
+func (c *delete) Process() error {
+	return nil
+}
+
+func (c *delete) Run(f *flag.FlagSet) error {
+	client, err := c.Client()
+	if err != nil {
+		return err
+	}
+
+	dc, err := c.Datacenter()
+	if err != nil {
+		return err
+	}
+
+	name, err := c.Name()
+	if err != nil {
+		return err
+	}
+
+	return client.FileManager().DeleteDatastoreFile(name, dc)
+}
