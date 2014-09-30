667df16abb7128f158ed5ee6350dd6944e08a62c
Doug MacEachern <dougm@vmware.com>
2014-09-30 14:32:55 -0700

    Add VCSA Vagrant box
    - script to create a Vagrant base box for vcsa
    
    - example vcsim (VC simulator) provisioner

---
+++ contrib/vagrant/vcsa/vagrant.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+useradd vagrant -m -s /bin/bash
+groupmod -A vagrant wheel
+
+echo -e "vagrant ALL=(ALL) NOPASSWD: ALL\n" >> /etc/sudoers
+
+mkdir ~vagrant/.ssh
+wget --no-check-certificate \
+     https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub \
+     -O ~vagrant/.ssh/authorized_keys
+chown -R vagrant ~vagrant/.ssh
+chmod -R go-rwsx ~vagrant/.ssh
+
+perl -pi -e 's/^#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+perl -pi -e 's/^AllowTcpForwarding no//' /etc/ssh/sshd_config
+perl -pi -e 's/^PermitTunnel no//' /etc/ssh/sshd_config
+perl -pi -e 's/^MaxSessions \d+//' /etc/ssh/sshd_config
