e4f4c737404d5f047228c6dea2f59fb3a6f2cabf
Bruce Downs <bdowns@vmware.com>
2015-09-02 10:04:22 -0600

    Misc clean up
    * change enumeration to be more idiomatic to golang

--- examples/datastores/main.go
+++ examples/datastores/main.go
@@ -64,11 +64,15 @@ func GetEnvBool(v string, def bool) bool {
 
 // Humanize converts a number in bytes to a more readable format.
 func Humanize(v int64) string {
-	const KB = 1024
-	const MB = 1024 * KB
-	const GB = 1024 * MB
-	const TB = 1024 * GB
-	const PB = 1024 * TB
+
+	const (
+		_  = iota
+		KB = 1 << (10 * iota)
+		MB
+		GB
+		TB
+		PB
+	)
 
 	switch {
 	case v < KB:
