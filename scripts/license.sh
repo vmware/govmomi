b1d9d3c2163fb9c72447ad3560135b57b7fbbf12
Pieter Noordhuis <pcnoordhuis@gmail.com>
2015-11-11 13:08:31 -0800

    Update copyright years in code headers

---
+++ scripts/license.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+header_dir=$(dirname $0)/headers
+
+tmpfile=$(mktemp)
+trap "rm -f ${tmpfile}" EXIT
+
+git ls-files | while read file; do
+  years=( $(git log --format='%ai' $file | cut -d- -f1 | sort -u) )
+  num_years=${#years[@]}
+  yearA=${years[0]}
+  yearB=${years[$((${num_years}-1))]}
+
+  if [ ${yearA} == ${yearB} ]; then
+    export YEARS="${yearA}"
+  else
+    export YEARS="${yearA}-${yearB}"
+  fi
+
+  case "$file" in
+    vim25/xml/*)
+      # Ignore
+      ;;
+    *.go)
+      sed -e "s/\${YEARS}/${YEARS}/" ${header_dir}/go.txt > ${tmpfile}
+      last_header_line=$(grep -n '\*/' ${file} | head -1 | cut -d: -f1)
+      tail -n +$((${last_header_line} + 1)) ${file} >> ${tmpfile}
+      mv ${tmpfile} ${file}
+      ;;
+    *.rb)
+      sed -e "s/\${YEARS}/${YEARS}/" ${header_dir}/rb.txt > ${tmpfile}
+      last_header_line=$(grep -n '^$' ${file} | head -1 | cut -d: -f1)
+      tail -n +$((${last_header_line})) ${file} >> ${tmpfile}
+      mv ${tmpfile} ${file}
+      ;;
+    *)
+      echo "Unhandled file: $file"
+      ;;
+  esac
+done
+
