vmware/govmomi

commit bfe414fe43430bd101e6bddd7fdff419063ebe58

Allow vm.create to take datastore cluster argument

object/virtual_device_list.go
@@ -349,7 +349,12 @@ func (l VirtualDeviceList) AssignController(device types.BaseVirtualDevice, c ty
 	d := device.GetVirtualDevice()
 	d.ControllerKey = c.GetVirtualController().Key
 	d.UnitNumber = l.newUnitNumber(c)
-	d.Key = -1
+	if d.UnitNumber == 0 {
+		d.UnitNumber = -1 // TODO: this field is annotated as omitempty
+	}
+	if d.Key == 0 {
+		d.Key = -1
+	}
 }
 
 // CreateDisk creates a new VirtualDisk device which can be added to a VM.
@@ -374,12 +379,6 @@ func (l VirtualDeviceList) CreateDisk(c types.BaseVirtualController, ds types.Ma
 	}
 
 	l.AssignController(device, c)
-
-	if device.UnitNumber == 0 {
-		device.UnitNumber = -1 // TODO: this field is annotated as omitempty
-	}
-
-	device.Key = l.NewKey()
 	return device
 }
 

govc/flags/storage_pod.go
@@ -1,0 +1,78 @@
+package flags
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"golang.org/x/net/context"
+
+	"github.com/vmware/govmomi/object"
+)
+
+type StoragePodFlag struct {
+	common
+
+	*DatacenterFlag
+
+	Name string
+
+	sp *object.StoragePod
+}
+
+var storagePodFlagKey = flagKey("storagePod")
+
+func NewStoragePodFlag(ctx context.Context) (*StoragePodFlag, context.Context) {
+	if v := ctx.Value(storagePodFlagKey); v != nil {
+		return v.(*StoragePodFlag), ctx
+	}
+
+	v := &StoragePodFlag{}
+	v.DatacenterFlag, ctx = NewDatacenterFlag(ctx)
+	ctx = context.WithValue(ctx, storagePodFlagKey, v)
+	return v, ctx
+}
+
+func (f *StoragePodFlag) Register(ctx context.Context, fs *flag.FlagSet) {
+	f.RegisterOnce(func() {
+		f.DatacenterFlag.Register(ctx, fs)
+
+		env := "GOVC_DATASTORE_CLUSTER"
+		value := os.Getenv(env)
+		usage := fmt.Sprintf("Datastore cluster [%s]", env)
+		fs.StringVar(&f.Name, "datastore-cluster", value, usage)
+	})
+}
+
+func (f *StoragePodFlag) Process(ctx context.Context) error {
+	return f.DatacenterFlag.Process(ctx)
+}
+
+func (f *StoragePodFlag) Isset() bool {
+	return f.Name != ""
+}
+
+func (f *StoragePodFlag) StoragePod() (*object.StoragePod, error) {
+	if f.sp != nil {
+		return f.sp, nil
+	}
+
+	finder, err := f.Finder()
+	if err != nil {
+		return nil, err
+	}
+
+	if f.Isset() {
+		f.sp, err = finder.DatastoreCluster(context.TODO(), f.Name)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		f.sp, err = finder.DefaultDatastoreCluster(context.TODO())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return f.sp, nil
+}

govc/test/vm.bats
@@ -218,7 +218,10 @@ load test_helper
 }
 
 @test "vm.create linked ide disk" {
+  import_ttylinux_vmdk
+
   vm=$(new_id)
+
   run govc vm.create -disk $GOVC_TEST_VMDK -disk.controller ide -on=false $vm
   assert_success
 
@@ -228,10 +231,12 @@ load test_helper
 }
 
 @test "vm.create linked scsi disk" {
+  import_ttylinux_vmdk
+
   vm=$(new_id)
 
   run govc vm.create -disk enoent -on=false $vm
-  assert_failure "govc: cannot stat '[${GOVC_DATASTORE}] enoent': No such file"
+  assert_failure "govc: cannot stat '[${GOVC_DATASTORE##*/}] enoent': No such file"
 
   run govc vm.create -disk $GOVC_TEST_VMDK -on=false $vm
   assert_success
@@ -239,15 +244,18 @@ load test_helper
   run govc device.info -vm $vm disk-1000-0
   assert_success
   assert_line "Controller: lsilogic-1000"
-  assert_line "Parent: [${GOVC_DATASTORE}] $GOVC_TEST_VMDK"
-  assert_line "File: [${GOVC_DATASTORE}] $vm/${vm}.vmdk"
+  assert_line "Parent: [${GOVC_DATASTORE##*/}] $GOVC_TEST_VMDK"
+  assert_line "File: [${GOVC_DATASTORE##*/}] $vm/${vm}.vmdk"
 }
 
 @test "vm.create scsi disk" {
+  import_ttylinux_vmdk
+
   vm=$(new_id)
 
   run govc vm.create -disk enoent -on=false $vm
-  assert_failure "govc: cannot stat '[${GOVC_DATASTORE}] enoent': No such file"
+  assert_failure "govc: cannot stat '[${GOVC_DATASTORE##*/}] enoent': No such file"
+
 
   run govc vm.create -disk $GOVC_TEST_VMDK -on=false -link=false $vm
   assert_success
@@ -255,11 +263,13 @@ load test_helper
   run govc device.info -vm $vm disk-1000-0
   assert_success
   assert_line "Controller: lsilogic-1000"
-  refute_line "Parent: [${GOVC_DATASTORE}] $GOVC_TEST_VMDK"
-  assert_line "File: [${GOVC_DATASTORE}] $GOVC_TEST_VMDK"
+  refute_line "Parent: [${GOVC_DATASTORE##*/}] $GOVC_TEST_VMDK"
+  assert_line "File: [${GOVC_DATASTORE##*/}] $GOVC_TEST_VMDK"
 }
 
 @test "vm.create scsi disk with datastore argument" {
+  import_ttylinux_vmdk
+
   vm=$(new_id)
 
   run govc vm.create -disk="${GOVC_TEST_VMDK}" -disk-datastore="${GOVC_DATASTORE}" -on=false -link=false $vm
@@ -267,7 +277,7 @@ load test_helper
 
   run govc device.info -vm $vm disk-1000-0
   assert_success
-  assert_line "File: [${GOVC_DATASTORE}] $GOVC_TEST_VMDK"
+  assert_line "File: [${GOVC_DATASTORE##*/}] $GOVC_TEST_VMDK"
 }
 
 @test "vm.create iso" {
@@ -276,7 +286,7 @@ load test_helper
   vm=$(new_id)
 
   run govc vm.create -iso enoent -on=false $vm
-  assert_failure "govc: cannot stat '[${GOVC_DATASTORE}] enoent': No such file"
+  assert_failure "govc: cannot stat '[${GOVC_DATASTORE##*/}] enoent': No such file"
 
   run govc vm.create -iso $GOVC_TEST_ISO -on=false $vm
   assert_success
@@ -284,7 +294,7 @@ load test_helper
   run govc device.info -vm $vm cdrom-3000
   assert_success
   assert_line "Controller: ide-200"
-  assert_line "Summary: ISO [${GOVC_DATASTORE}] $GOVC_TEST_ISO"
+  assert_line "Summary: ISO [${GOVC_DATASTORE##*/}] $GOVC_TEST_ISO"
 }
 
 @test "vm.create iso with datastore argument" {
@@ -297,7 +307,7 @@ load test_helper
 
   run govc device.info -vm $vm cdrom-3000
   assert_success
-  assert_line "Summary: ISO [${GOVC_DATASTORE}] $GOVC_TEST_ISO"
+  assert_line "Summary: ISO [${GOVC_DATASTORE##*/}] $GOVC_TEST_ISO"
 }
 
 @test "vm.disk.create empty vm" {
@@ -320,6 +330,7 @@ load test_helper
 
 @test "vm.disk.create" {
   import_ttylinux_vmdk
+
   vm=$(new_id)
 
   govc vm.create -disk $GOVC_TEST_VMDK -on=false $vm
@@ -341,6 +352,7 @@ load test_helper
 
 @test "vm.disk.attach" {
   import_ttylinux_vmdk
+
   vm=$(new_id)
 
   govc vm.create -disk $GOVC_TEST_VMDK -on=false $vm
@@ -351,7 +363,7 @@ load test_helper
   assert_success
 
   run govc vm.disk.attach -vm $vm -link=false -disk enoent.vmdk
-  assert_failure "govc: File [${GOVC_DATASTORE}] enoent.vmdk was not found"
+  assert_failure "govc: File [${GOVC_DATASTORE##*/}] enoent.vmdk was not found"
 
   run govc vm.disk.attach -vm $vm -disk enoent.vmdk
   assert_failure "govc: Invalid configuration for device '0'."
@@ -360,4 +372,29 @@ load test_helper
   assert_success
   result=$(govc device.ls -vm $vm | grep disk- | wc -l)
   [ $result -eq 2 ]
+}
+
+@test "vm.create new disk with datastore argument" {
+  vm=$(new_id)
+
+  run govc vm.create -disk="1GiB" -ds="${GOVC_DATASTORE}" -on=false -link=false $vm
+  assert_success
+
+  run govc device.info -vm $vm disk-1000-0
+  assert_success
+  assert_line "File: [${GOVC_DATASTORE##*/}] ${vm}/${vm}.vmdk"
+}
+
+@test "vm.create new disk with datastore cluster argument" {
+  if [ -z "${GOVC_DATASTORE_CLUSTER}" ]; then
+    skip "requires datastore cluster"
+  fi
+
+  vm=$(new_id)
+
+  run govc vm.create -disk="1GiB" -datastore-cluster="${GOVC_DATASTORE_CLUSTER}" -on=false -link=false $vm
+  assert_success
+
+  run govc device.info -vm $vm disk-1000-0
+  assert_success
 }

govc/vm/create.go
@@ -23,7 +23,10 @@ import (
 	"github.com/vmware/govmomi/govc/cli"
 	"github.com/vmware/govmomi/govc/flags"
 	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/property"
+	"github.com/vmware/govmomi/units"
 	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/vim25/types"
 	"golang.org/x/net/context"
 )
@@ -32,6 +35,7 @@ type create struct {
 	*flags.ClientFlag
 	*flags.DatacenterFlag
 	*flags.DatastoreFlag
+	*flags.StoragePodFlag
 	*flags.ResourcePoolFlag
 	*flags.HostSystemFlag
 	*flags.NetworkFlag
@@ -53,9 +57,14 @@ type create struct {
 	diskDatastoreFlag *flags.DatastoreFlag
 	diskDatastore     *object.Datastore
 
+	// Only set if the disk argument is a byte size, which means the disk
+	// doesn't exist yet and should be created
+	diskByteSize int64
+
 	Client       *vim25.Client
 	Datacenter   *object.Datacenter
 	Datastore    *object.Datastore
+	StoragePod   *object.StoragePod
 	ResourcePool *object.ResourcePool
 	HostSystem   *object.HostSystem
 }
@@ -73,6 +82,9 @@ func (cmd *create) Register(ctx context.Context, f *flag.FlagSet) {
 
 	cmd.DatastoreFlag, ctx = flags.NewDatastoreFlag(ctx)
 	cmd.DatastoreFlag.Register(ctx, f)
+
+	cmd.StoragePodFlag, ctx = flags.NewStoragePodFlag(ctx)
+	cmd.StoragePodFlag.Register(ctx, f)
 
 	cmd.ResourcePoolFlag, ctx = flags.NewResourcePoolFlag(ctx)
 	cmd.ResourcePoolFlag.Register(ctx, f)
@@ -95,7 +107,7 @@ func (cmd *create) Register(ctx context.Context, f *flag.FlagSet) {
 	cmd.isoDatastoreFlag, ctx = flags.NewCustomDatastoreFlag(ctx)
 	f.StringVar(&cmd.isoDatastoreFlag.Name, "iso-datastore", "", "Datastore for ISO file")
 
-	f.StringVar(&cmd.disk, "disk", "", "Disk path")
+	f.StringVar(&cmd.disk, "disk", "", "Disk path (to use existing) OR size (to create new, e.g. 20GB)")
 	cmd.diskDatastoreFlag, ctx = flags.NewCustomDatastoreFlag(ctx)
 	f.StringVar(&cmd.diskDatastoreFlag.Name, "disk-datastore", "", "Datastore for disk file")
 }
@@ -108,6 +120,9 @@ func (cmd *create) Process(ctx context.Context) error {
 		return err
 	}
 	if err := cmd.DatastoreFlag.Process(ctx); err != nil {
+		return err
+	}
+	if err := cmd.StoragePodFlag.Process(ctx); err != nil {
 		return err
 	}
 	if err := cmd.ResourcePoolFlag.Process(ctx); err != nil {
@@ -153,9 +168,16 @@ func (cmd *create) Run(ctx context.Context, f *flag.FlagSet) error {
 		return err
 	}
 
-	cmd.Datastore, err = cmd.DatastoreFlag.Datastore()
-	if err != nil {
-		return err
+	if cmd.StoragePodFlag.Isset() {
+		cmd.StoragePod, err = cmd.StoragePodFlag.StoragePod()
+		if err != nil {
+			return err
+		}
+	} else {
+		cmd.Datastore, err = cmd.DatastoreFlag.Datastore()
+		if err != nil {
+			return err
+		}
 	}
 
 	cmd.HostSystem, err = cmd.HostSystemFlag.HostSystemIfSpecified()
@@ -189,14 +211,22 @@ func (cmd *create) Run(ctx context.Context, f *flag.FlagSet) error {
 
 	// Verify disk exists
 	if cmd.disk != "" {
-		_, err = cmd.diskDatastoreFlag.Stat(context.TODO(), cmd.disk)
-		if err != nil {
-			return err
-		}
+		var b units.ByteSize
 
-		cmd.diskDatastore, err = cmd.diskDatastoreFlag.Datastore()
-		if err != nil {
-			return err
+		// If disk can be parsed as byte units, don't stat
+		err = b.Set(cmd.disk)
+		if err == nil {
+			cmd.diskByteSize = int64(b)
+		} else {
+			_, err = cmd.diskDatastoreFlag.Stat(context.TODO(), cmd.disk)
+			if err != nil {
+				return err
+			}
+
+			cmd.diskDatastore, err = cmd.diskDatastoreFlag.Datastore()
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -231,10 +261,9 @@ func (cmd *create) createVM(ctx context.Context) (*object.Task, error) {
 	var devices object.VirtualDeviceList
 	var err error
 
-	spec := types.VirtualMachineConfigSpec{
+	spec := &types.VirtualMachineConfigSpec{
 		Name:     cmd.name,
 		GuestId:  cmd.guestID,
-		Files:    &types.VirtualMachineFileInfo{VmPathName: fmt.Sprintf("[%s]", cmd.Datastore.Name())},
 		NumCPUs:  cmd.cpus,
 		MemoryMB: int64(cmd.memory),
 	}
@@ -256,10 +285,22 @@ func (cmd *create) createVM(ctx context.Context) (*object.Task, error) {
 
 	spec.DeviceChange = deviceChange
 
+	var datastore *object.Datastore
+
+	// If storage pod is specified, collect placement recommendations
+	if cmd.StoragePod != nil {
+		datastore, err = cmd.recommendDatastore(ctx, spec)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		datastore = cmd.Datastore
+	}
+
 	if !cmd.force {
 		vmxPath := fmt.Sprintf("%s/%s.vmx", cmd.name, cmd.name)
 
-		_, err := cmd.Datastore.Stat(ctx, vmxPath)
+		_, err := datastore.Stat(ctx, vmxPath)
 		if err == nil {
 			dsPath := cmd.Datastore.Path(vmxPath)
 			return nil, fmt.Errorf("File %s already exists", dsPath)
@@ -271,7 +312,11 @@ func (cmd *create) createVM(ctx context.Context) (*object.Task, error) {
 		return nil, err
 	}
 
-	return folders.VmFolder.CreateVM(ctx, spec, cmd.ResourcePool, cmd.HostSystem)
+	spec.Files = &types.VirtualMachineFileInfo{
+		VmPathName: fmt.Sprintf("[%s]", datastore.Name()),
+	}
+
+	return folders.VmFolder.CreateVM(ctx, *spec, cmd.ResourcePool, cmd.HostSystem)
 }
 
 func (cmd *create) addStorage(devices object.VirtualDeviceList) (object.VirtualDeviceList, error) {
@@ -294,7 +339,26 @@ func (cmd *create) addStorage(devices object.VirtualDeviceList) (object.VirtualD
 		devices = append(devices, ide)
 	}
 
-	if cmd.disk != "" {
+	if cmd.diskByteSize != 0 {
+		controller, err := devices.FindDiskController(cmd.controller)
+		if err != nil {
+			return nil, err
+		}
+
+		disk := &types.VirtualDisk{
+			VirtualDevice: types.VirtualDevice{
+				Key: devices.NewKey(),
+				Backing: &types.VirtualDiskFlatVer2BackingInfo{
+					DiskMode:        string(types.VirtualDiskModePersistent),
+					ThinProvisioned: types.NewBool(true),
+				},
+			},
+			CapacityInKB: cmd.diskByteSize / 1024,
+		}
+
+		devices.AssignController(disk, controller)
+		devices = append(devices, disk)
+	} else if cmd.disk != "" {
 		controller, err := devices.FindDiskController(cmd.controller)
 		if err != nil {
 			return nil, err
@@ -337,4 +401,84 @@ func (cmd *create) addNetwork(devices object.VirtualDeviceList) (object.VirtualD
 
 	devices = append(devices, netdev)
 	return devices, nil
+}
+
+func (cmd *create) recommendDatastore(ctx context.Context, spec *types.VirtualMachineConfigSpec) (*object.Datastore, error) {
+	sp := cmd.StoragePod.Reference()
+
+	// Build pod selection spec from config spec
+	podSelectionSpec := types.StorageDrsPodSelectionSpec{
+		StoragePod: &sp,
+	}
+
+	// Keep list of disks that need to be placed
+	var disks []*types.VirtualDisk
+
+	// Collect disks eligible for placement
+	for _, deviceConfigSpec := range spec.DeviceChange {
+		s := deviceConfigSpec.GetVirtualDeviceConfigSpec()
+		if s.Operation != types.VirtualDeviceConfigSpecOperationAdd {
+			continue
+		}
+
+		if s.FileOperation != types.VirtualDeviceConfigSpecFileOperationCreate {
+			continue
+		}
+
+		d, ok := s.Device.(*types.VirtualDisk)
+		if !ok {
+			continue
+		}
+
+		podConfigForPlacement := types.VmPodConfigForPlacement{
+			StoragePod: sp,
+			Disk: []types.PodDiskLocator{
+				{
+					DiskId:          d.Key,
+					DiskBackingInfo: d.Backing,
+				},
+			},
+		}
+
+		podSelectionSpec.InitialVmConfig = append(podSelectionSpec.InitialVmConfig, podConfigForPlacement)
+		disks = append(disks, d)
+	}
+
+	sps := types.StoragePlacementSpec{
+		Type:             string(types.StoragePlacementSpecPlacementTypeCreate),
+		ResourcePool:     types.NewReference(cmd.ResourcePool.Reference()),
+		PodSelectionSpec: podSelectionSpec,
+		ConfigSpec:       spec,
+	}
+
+	srm := object.NewStorageResourceManager(cmd.Client)
+	result, err := srm.RecommendDatastores(ctx, sps)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use result to pin disks to recommended datastores
+	recs := result.Recommendations
+	if len(recs) == 0 {
+		return nil, fmt.Errorf("no recommendations")
+	}
+
+	ds := recs[0].Action[0].(*types.StoragePlacementAction).Destination
+
+	var mds mo.Datastore
+	err = property.DefaultCollector(cmd.Client).RetrieveOne(ctx, ds, []string{"name"}, &mds)
+	if err != nil {
+		return nil, err
+	}
+
+	datastore := object.NewDatastore(cmd.Client, ds)
+	datastore.InventoryPath = mds.Name
+
+	// Apply recommendation to eligible disks
+	for _, disk := range disks {
+		backing := disk.Backing.(*types.VirtualDiskFlatVer2BackingInfo)
+		backing.Datastore = &ds
+	}
+
+	return datastore, nil
 }

vim25/types/helpers.go
@@ -19,3 +19,7 @@ package types
 func NewBool(v bool) *bool {
 	return &v
 }
+
+func NewReference(r ManagedObjectReference) *ManagedObjectReference {
+	return &r
+}

commit dff2c197be0945c23ed354dccee76679cedc3162

Fix VirtualDeviceList.CreateFloppy
device.floppy.add related tests have been failing since bfe414f

object/virtual_device_list.go
@@ -350,9 +350,6 @@ func (l VirtualDeviceList) AssignController(device types.BaseVirtualDevice, c ty
 	d.ControllerKey = c.GetVirtualController().Key
 	d.UnitNumber = new(int)
 	*d.UnitNumber = l.newUnitNumber(c)
-	if *d.UnitNumber == 0 {
-		*d.UnitNumber = -1
-	}
 	if d.Key == 0 {
 		d.Key = -1
 	}