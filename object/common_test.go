vmware/govmomi

commit 558321dfeac7ffa1f8b8c7b1f6d234442f49523b

Move InventoryPath field to object.Common

object/common.go
@@ -19,10 +19,12 @@ package object
 import (
 	"errors"
 	"fmt"
+	"path"
 
 	"github.com/vmware/govmomi/property"
 	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/methods"
+	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/vim25/types"
 	"golang.org/x/net/context"
 )
@@ -33,12 +35,20 @@ var (
 
 // Common contains the fields and functions common to all objects.
 type Common struct {
+	InventoryPath string
+
 	c *vim25.Client
 	r types.ManagedObjectReference
 }
 
 func (c Common) String() string {
-	return fmt.Sprintf("%v", c.Reference())
+	ref := fmt.Sprintf("%v", c.Reference())
+
+	if c.InventoryPath == "" {
+		return ref
+	}
+
+	return fmt.Sprintf("%s @ %s", ref, c.InventoryPath)
 }
 
 func NewCommon(c *vim25.Client, r types.ManagedObjectReference) Common {
@@ -51,6 +61,29 @@ func (c Common) Reference() types.ManagedObjectReference {
 
 func (c Common) Client() *vim25.Client {
 	return c.c
+}
+
+// Name returns the base name of the InventoryPath field
+func (c Common) Name() string {
+	return path.Base(c.InventoryPath)
+}
+
+// ObjectName returns the base name of the InventoryPath field if set,
+// otherwise fetches the mo.ManagedEntity.Name field via the property collector.
+func (c Common) ObjectName(ctx context.Context) (string, error) {
+	var o mo.ManagedEntity
+
+	name := c.Name()
+	if name != "" {
+		return name, nil
+	}
+
+	err := c.Properties(ctx, c.Reference(), []string{"name"}, &o)
+	if err != nil {
+		return "", err
+	}
+
+	return o.Name, nil
 }
 
 func (c Common) Properties(ctx context.Context, r types.ManagedObjectReference, ps []string, dst interface{}) error {

find/finder.go
@@ -210,7 +210,9 @@ func (f *Finder) DatacenterList(ctx context.Context, path string) ([]*object.Dat
 	for _, e := range es {
 		ref := e.Object.Reference()
 		if ref.Type == "Datacenter" {
-			dcs = append(dcs, object.NewDatacenter(f.client, ref))
+			dc := object.NewDatacenter(f.client, ref)
+			dc.InventoryPath = e.Path
+			dcs = append(dcs, dc)
 		}
 	}
 

object/cluster_compute_resource.go
@@ -25,8 +25,6 @@ import (
 
 type ClusterComputeResource struct {
 	ComputeResource
-
-	InventoryPath string
 }
 
 func NewClusterComputeResource(c *vim25.Client, ref types.ManagedObjectReference) *ClusterComputeResource {

object/compute_resource.go
@@ -29,8 +29,6 @@ import (
 
 type ComputeResource struct {
 	Common
-
-	InventoryPath string
 }
 
 func NewComputeResource(c *vim25.Client, ref types.ManagedObjectReference) *ComputeResource {

object/datastore.go
@@ -57,18 +57,12 @@ func (e DatastoreNoSuchFileError) Error() string {
 
 type Datastore struct {
 	Common
-
-	InventoryPath string
 }
 
 func NewDatastore(c *vim25.Client, ref types.ManagedObjectReference) *Datastore {
 	return &Datastore{
 		Common: NewCommon(c, ref),
 	}
-}
-
-func (d Datastore) Name() string {
-	return path.Base(d.InventoryPath)
 }
 
 func (d Datastore) Path(path string) string {
@@ -142,7 +136,7 @@ func (d Datastore) ServiceTicket(ctx context.Context, path string, method string
 
 		// Pick a random attached host
 		host := hosts[rand.Intn(len(hosts))]
-		name, err := host.Name(ctx)
+		name, err := host.ObjectName(ctx)
 		if err != nil {
 			return nil, nil, err
 		}

object/distributed_virtual_portgroup.go
@@ -17,8 +17,6 @@ limitations under the License.
 package object
 
 import (
-	"path"
-
 	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/vim25/types"
@@ -27,17 +25,12 @@ import (
 
 type DistributedVirtualPortgroup struct {
 	Common
-
-	InventoryPath string
 }
 
 func NewDistributedVirtualPortgroup(c *vim25.Client, ref types.ManagedObjectReference) *DistributedVirtualPortgroup {
 	return &DistributedVirtualPortgroup{
 		Common: NewCommon(c, ref),
 	}
-}
-func (p DistributedVirtualPortgroup) Name() string {
-	return path.Base(p.InventoryPath)
 }
 
 // EthernetCardBackingInfo returns the VirtualDeviceBackingInfo for this DistributedVirtualPortgroup

object/distributed_virtual_switch.go
@@ -25,8 +25,6 @@ import (
 
 type DistributedVirtualSwitch struct {
 	Common
-
-	InventoryPath string
 }
 
 func NewDistributedVirtualSwitch(c *vim25.Client, ref types.ManagedObjectReference) *DistributedVirtualSwitch {

object/folder.go
@@ -26,8 +26,6 @@ import (
 
 type Folder struct {
 	Common
-
-	InventoryPath string
 }
 
 func NewFolder(c *vim25.Client, ref types.ManagedObjectReference) *Folder {

commit 2cad28d07f77e54038f8e8a2de859a185534569b

Fix Datastore upload/download against VC
These methods need a service ticket, which when run against VC uses the
ObjectName of the selected HostSystem as the URL host.  The Name()
method was returning path.Base(InventoryPath), which defaults to ".",
when we should return an empty string in that case.

Caused by commit 558321d

govc/version/command.go
@@ -27,7 +27,7 @@ import (
 	"github.com/vmware/govmomi/govc/flags"
 )
 
-const Version = "0.7.0"
+const Version = "0.7.1"
 
 var gitVersion string
 

object/common.go
@@ -65,6 +65,9 @@ func (c Common) Client() *vim25.Client {
 
 // Name returns the base name of the InventoryPath field
 func (c Common) Name() string {
+	if c.InventoryPath == "" {
+		return ""
+	}
 	return path.Base(c.InventoryPath)
 }
 

object/common_test.go
@@ -1,0 +1,34 @@
+/*
+Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package object
+
+import "testing"
+
+func TestCommonName(t *testing.T) {
+	c := &Common{}
+
+	name := c.Name()
+	if name != "" {
+		t.Errorf("Name=%s", name)
+	}
+
+	c.InventoryPath = "/foo/bar"
+	name = c.Name()
+	if name != "bar" {
+		t.Errorf("Name=%s", name)
+	}
+}