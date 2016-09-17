1e52d88a5313b62bac329bb00ef63da2462f5d9a
Doug MacEachern <dougm@vmware.com>
2016-09-17 15:52:46 -0700

    Update contributors

--- scripts/contributors.sh
+++ scripts/contributors.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# contributors with > 1 email
+dups=(abathla@promb-1s-dhcp216.eng.vmware.com
+      cedric.blomart@minfin.fed.be
+      david.stark@bskyb.com
+      egorovv@gmail.com)
+
 outfile="CONTRIBUTORS"
 tmpfile="CONTRIBUTORS.tmp"
 cp "${outfile}" "${tmpfile}"
@@ -10,6 +16,9 @@ cp "${outfile}" "${tmpfile}"
 git shortlog -sne | while read line; do
   name=$(perl -pe 's/\d+\s+//' <<<"${line}")
   email=$(grep -Po '(?<=<).*(?=>)' <<<"${name}")
+  if grep -q "$email" <<<"${dups[@]}"; then
+    continue
+  fi
   if ! grep -q "${email}" "${outfile}"; then
     echo "${name}" >> "${tmpfile}"
   fi
