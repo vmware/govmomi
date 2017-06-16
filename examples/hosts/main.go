94837bf7a3bf9bcc044b43893d5cc1f279a4f0a3
Doug MacEachern <dougm@vmware.com>
2017-06-16 12:39:42 -0700

    Update examples
    - Add Logout + container view Destroy
    
    - Move common code to examples.NewClient method
    
    - Add examples/networks

--- examples/hosts/main.go
+++ examples/hosts/main.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 /*
-This example program shows how the `finder` and `property` packages can
+This example program shows how the `view` and `property` packages can
 be used to navigate a vSphere inventory structure using govmomi.
 */
 
@@ -23,146 +23,65 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
-	"net/url"
+	"log"
 	"os"
-	"strings"
 	"text/tabwriter"
 
-	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/examples"
 	"github.com/vmware/govmomi/units"
 	"github.com/vmware/govmomi/view"
 	"github.com/vmware/govmomi/vim25/mo"
 )
 
-// GetEnvString returns string from environment variable.
-func GetEnvString(v string, def string) string {
-	r := os.Getenv(v)
-	if r == "" {
-		return def
-	}
-
-	return r
-}
-
-// GetEnvBool returns boolean from environment variable.
-func GetEnvBool(v string, def bool) bool {
-	r := os.Getenv(v)
-	if r == "" {
-		return def
-	}
-
-	switch strings.ToLower(r[0:1]) {
-	case "t", "y", "1":
-		return true
-	}
-
-	return false
-}
-
-const (
-	envURL      = "GOVMOMI_URL"
-	envUserName = "GOVMOMI_USERNAME"
-	envPassword = "GOVMOMI_PASSWORD"
-	envInsecure = "GOVMOMI_INSECURE"
-)
-
-var urlDescription = fmt.Sprintf("ESX or vCenter URL [%s]", envURL)
-var urlFlag = flag.String("url", GetEnvString(envURL, "https://username:password@host/sdk"), urlDescription)
-
-var insecureDescription = fmt.Sprintf("Don't verify the server's certificate chain [%s]", envInsecure)
-var insecureFlag = flag.Bool("insecure", GetEnvBool(envInsecure, false), insecureDescription)
-
-func processOverride(u *url.URL) {
-	envUsername := os.Getenv(envUserName)
-	envPassword := os.Getenv(envPassword)
-
-	// Override username if provided
-	if envUsername != "" {
-		var password string
-		var ok bool
-
-		if u.User != nil {
-			password, ok = u.User.Password()
-		}
-
-		if ok {
-			u.User = url.UserPassword(envUsername, password)
-		} else {
-			u.User = url.User(envUsername)
-		}
-	}
-
-	// Override password if provided
-	if envPassword != "" {
-		var username string
-
-		if u.User != nil {
-			username = u.User.Username()
-		}
-
-		u.User = url.UserPassword(username, envPassword)
-	}
-}
-
-func exit(err error) {
-	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-	os.Exit(1)
-}
-
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	flag.Parse()
-
-	// Parse URL from string
-	u, err := url.Parse(*urlFlag)
+	// Connect and login to ESX or vCenter
+	c, err := examples.NewClient(ctx)
 	if err != nil {
-		exit(err)
+		log.Fatal(err)
 	}
 
-	// Override username and/or password as required
-	processOverride(u)
+	defer c.Logout(ctx)
 
-	// Connect and log in to ESX or vCenter
-	c, err := govmomi.NewClient(ctx, u, *insecureFlag)
-	if err != nil {
-		exit(err)
-	}
-
-	// Get view of HostSystem
+	// Create a view of HostSystem objects
 	m := view.NewManager(c.Client)
 
 	v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"HostSystem"}, true)
 	if err != nil {
-		exit(err)
+		log.Fatal(err)
 	}
 
-	// Reference for this data object:
-	// https://www.vmware.com/support/developer/vc-sdk/visdk25pubs/ReferenceGuide/vim.host.Summary.html
+	defer v.Destroy(ctx)
+
+	// Retrieve summary property for all hosts
+	// Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.HostSystem.html
 	var hss []mo.HostSystem
 	err = v.Retrieve(ctx, []string{"HostSystem"}, []string{"summary"}, &hss)
 	if err != nil {
-		exit(err)
+		log.Fatal(err)
 	}
 
-	// Print summary per host
+	// Print summary per host (see also: govc/host/info.go)
+
 	tw := tabwriter.NewWriter(os.Stdout, 2, 0, 2, ' ', 0)
 	fmt.Fprintf(tw, "Name:\tUsed CPU:\tTotal CPU:\tFree CPU:\tUsed Memory:\tTotal Memory:\tFree Memory\t:\n")
+
 	for _, hs := range hss {
-		totalCpu := int64(hs.Summary.Hardware.CpuMhz) * int64(hs.Summary.Hardware.NumCpuCores)
-		freeCpu := int64(totalCpu) - int64(hs.Summary.QuickStats.OverallCpuUsage)
+		totalCPU := int64(hs.Summary.Hardware.CpuMhz) * int64(hs.Summary.Hardware.NumCpuCores)
+		freeCPU := int64(totalCPU) - int64(hs.Summary.QuickStats.OverallCpuUsage)
 		freeMemory := int64(hs.Summary.Hardware.MemorySize) - (int64(hs.Summary.QuickStats.OverallMemoryUsage) * 1024 * 1024)
 		fmt.Fprintf(tw, "%s\t", hs.Summary.Config.Name)
 		fmt.Fprintf(tw, "%d\t", hs.Summary.QuickStats.OverallCpuUsage)
-		fmt.Fprintf(tw, "%d\t", totalCpu)
-		fmt.Fprintf(tw, "%d\t", freeCpu)
+		fmt.Fprintf(tw, "%d\t", totalCPU)
+		fmt.Fprintf(tw, "%d\t", freeCPU)
 		fmt.Fprintf(tw, "%s\t", units.ByteSize(hs.Summary.QuickStats.OverallMemoryUsage))
 		fmt.Fprintf(tw, "%s\t", units.ByteSize(hs.Summary.Hardware.MemorySize))
 		fmt.Fprintf(tw, "%d\t", freeMemory)
 		fmt.Fprintf(tw, "\n")
 	}
-	tw.Flush()
+
+	_ = tw.Flush()
 }
