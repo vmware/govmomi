f4cead22f3765ec3d9a9e009a6ceaeb9598d3a73
Trevor Dawe <trevor.dawe@gmail.com>
2018-01-08 12:25:12 -0400

    Added AttachScsiLun function
    Added AttachScsiLun function to host_storage_system

--- object/host_storage_system.go
+++ object/host_storage_system.go
@@ -161,3 +161,14 @@ func (s HostStorageSystem) MarkAsNonLocal(ctx context.Context, uuid string) (*Ta
 
 	return NewTask(s.c, res.Returnval), nil
 }
+
+func (s HostStorageSystem) AttachScsiLun(ctx context.Context, uuid string) (*types.AttachScsiLunResponse, error) {
+	req := types.AttachScsiLun{
+		This:    s.Reference(),
+		LunUuid: uuid,
+	}
+
+	res, err := methods.AttachScsiLun(ctx, s.c, &req)
+
+	return res, err
+}
