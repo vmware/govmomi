vmware/govmomi

commit 585cf5e143e8d1df60ef2f20ba3a1ae46e8eeb3e

vcsim: add portgroup related operations

govc/dvs/portgroup/info.go
@@ -24,7 +24,6 @@ import (
 	"github.com/vmware/govmomi/govc/cli"
 	"github.com/vmware/govmomi/govc/flags"
 	"github.com/vmware/govmomi/object"
-	"github.com/vmware/govmomi/vim25/methods"
 	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/vim25/types"
 )
@@ -83,11 +82,6 @@ func (cmd *info) Run(ctx context.Context, f *flag.FlagSet) error {
 		return flag.ErrHelp
 	}
 
-	client, err := cmd.Client()
-	if err != nil {
-		return err
-	}
-
 	finder, err := cmd.Finder()
 	if err != nil {
 		return err
@@ -137,14 +131,7 @@ func (cmd *info) Run(ctx context.Context, f *flag.FlagSet) error {
 		criteria.PortgroupKey = []string{dvp.Key}
 	}
 
-	// Prepare request
-	req := types.FetchDVPorts{
-		This:     dvs.Reference(),
-		Criteria: &criteria,
-	}
-
-	// Fetch ports
-	res, err := methods.FetchDVPorts(ctx, client, &req)
+	res, err := dvs.FetchDVPorts(ctx)
 	if err != nil {
 		return err
 	}
@@ -152,7 +139,7 @@ func (cmd *info) Run(ctx context.Context, f *flag.FlagSet) error {
 	var returnedPorts uint
 
 	// Iterate over returned ports
-	for _, port := range res.Returnval {
+	for _, port := range res {
 		portConfigSetting := port.Config.Setting.(*types.VMwareDVSPortSetting)
 		portVlan := portConfigSetting.Vlan.(*types.VmwareDistributedVirtualSwitchVlanIdSpec)
 		portVlanID := portVlan.VlanId

object/distributed_virtual_switch.go
@@ -65,3 +65,15 @@ func (s DistributedVirtualSwitch) AddPortgroup(ctx context.Context, spec []types
 
 	return NewTask(s.Client(), res.Returnval), nil
 }
+
+func (s DistributedVirtualSwitch) FetchDVPorts(ctx context.Context) ([]types.DistributedVirtualPort, error) {
+	req := &types.FetchDVPorts{
+		This: s.Reference(),
+	}
+
+	res, err := methods.FetchDVPorts(ctx, s.Client(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res.Returnval, nil
+}

simulator/dvs.go
@@ -32,8 +32,9 @@ func (s *DistributedVirtualSwitch) AddDVPortgroupTask(c *types.AddDVPortgroup_Ta
 		f := Map.getEntityParent(s, "Folder").(*Folder)
 
 		for _, spec := range c.Spec {
-			pg := &mo.DistributedVirtualPortgroup{}
+			pg := &DistributedVirtualPortgroup{}
 			pg.Name = spec.Name
+			pg.Config.DefaultPortConfig = spec.DefaultPortConfig
 			pg.Entity().Name = pg.Name
 
 			if obj := Map.FindByName(pg.Name, f.ChildEntity); obj != nil {
@@ -93,7 +94,7 @@ func (s *DistributedVirtualSwitch) ReconfigureDvsTask(req *types.ReconfigureDvs_
 				s.Summary.HostMember = append(s.Summary.HostMember, member.Host)
 
 				for _, ref := range s.Portgroup {
-					pg := Map.Get(ref).(*mo.DistributedVirtualPortgroup)
+					pg := Map.Get(ref).(*DistributedVirtualPortgroup)
 					pg.Host = AddReference(member.Host, pg.Host)
 				}
 			case types.ConfigSpecOperationRemove:
@@ -121,4 +122,58 @@ func (s *DistributedVirtualSwitch) ReconfigureDvsTask(req *types.ReconfigureDvs_
 			Returnval: task.Self,
 		},
 	}
+}
+
+func (s *DistributedVirtualSwitch) FetchDVPorts(req *types.FetchDVPorts) soap.HasFault {
+	body := &methods.FetchDVPortsBody{}
+	body.Res = &types.FetchDVPortsResponse{
+		Returnval: s.dvPortgroups(req.Criteria),
+	}
+	return body
+}
+
+func (s *DistributedVirtualSwitch) DestroyTask(req *types.Destroy_Task) soap.HasFault {
+	task := CreateTask(s, "destroy", func(t *Task) (types.AnyType, types.BaseMethodFault) {
+		f := Map.getEntityParent(s, "Folder").(*Folder)
+		f.removeChild(s.Reference())
+		return nil, nil
+	})
+
+	task.Run()
+
+	return &methods.Destroy_TaskBody{
+		Res: &types.Destroy_TaskResponse{
+			Returnval: task.Self,
+		},
+	}
+}
+
+func (s *DistributedVirtualSwitch) dvPortgroups(_ *types.DistributedVirtualSwitchPortCriteria) []types.DistributedVirtualPort {
+	// TODO(agui): Filter is not implemented yet
+	var res []types.DistributedVirtualPort
+	for _, ref := range s.Portgroup {
+		pg := Map.Get(ref).(*DistributedVirtualPortgroup)
+		res = append(res, types.DistributedVirtualPort{
+			DvsUuid: s.Uuid,
+			Key:     pg.Key,
+			Config: types.DVPortConfigInfo{
+				Setting: pg.Config.DefaultPortConfig,
+			},
+		})
+
+		if pg.PortKeys == nil {
+			continue
+		}
+
+		for _, key := range pg.PortKeys {
+			res = append(res, types.DistributedVirtualPort{
+				DvsUuid: s.Uuid,
+				Key:     key,
+				Config: types.DVPortConfigInfo{
+					Setting: pg.Config.DefaultPortConfig,
+				},
+			})
+		}
+	}
+	return res
 }

govc/test/network.bats
@@ -150,7 +150,7 @@ load test_helper
 }
 
 @test "dvs.portgroup" {
-  vcsim_env_todo
+  vcsim_env
   id=$(new_id)
 
   run govc dvs.create "$id"

simulator/dvs_test.go
@@ -128,4 +128,22 @@ func TestDVS(t *testing.T) {
 			t.Errorf("expected %T fault in test %d", test.err, x)
 		}
 	}
+
+	ports, err := dvs.FetchDVPorts(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(ports) != 1 {
+		t.Fatalf("expected 1 ports in DVPorts; got %d", len(ports))
+	}
+
+	dtask, err = dvs.Destroy(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = dtask.Wait(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
 }

simulator/portgroup.go
@@ -1,0 +1,86 @@
+/*
+Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package simulator
+
+import (
+	"github.com/vmware/govmomi/vim25/methods"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/soap"
+	"github.com/vmware/govmomi/vim25/types"
+)
+
+type DistributedVirtualPortgroup struct {
+	mo.DistributedVirtualPortgroup
+}
+
+func (s *DistributedVirtualPortgroup) ReconfigureDVPortgroupTask(req *types.ReconfigureDVPortgroup_Task) soap.HasFault {
+	task := CreateTask(s, "reconfigureDvPortgroup", func(t *Task) (types.AnyType, types.BaseMethodFault) {
+		s.Config.DefaultPortConfig = req.Spec.DefaultPortConfig
+		s.Config.NumPorts = req.Spec.NumPorts
+		s.Config.AutoExpand = req.Spec.AutoExpand
+		s.Config.Type = req.Spec.Type
+		s.Config.Description = req.Spec.Description
+		s.Config.DynamicData = req.Spec.DynamicData
+		s.Config.Name = req.Spec.Name
+		s.Config.Policy = req.Spec.Policy
+		s.Config.PortNameFormat = req.Spec.PortNameFormat
+		s.Config.VmVnicNetworkResourcePoolKey = req.Spec.VmVnicNetworkResourcePoolKey
+
+		return nil, nil
+	})
+
+	task.Run()
+
+	return &methods.ReconfigureDVPortgroup_TaskBody{
+		Res: &types.ReconfigureDVPortgroup_TaskResponse{
+			Returnval: task.Self,
+		},
+	}
+}
+
+func (s *DistributedVirtualPortgroup) DestroyTask(req *types.Destroy_Task) soap.HasFault {
+	task := CreateTask(s, "destroy", func(t *Task) (types.AnyType, types.BaseMethodFault) {
+		vswitch := Map.Get(*s.Config.DistributedVirtualSwitch).(*DistributedVirtualSwitch)
+		for i, pg := range vswitch.Portgroup {
+			if pg.Reference() == s.Reference() {
+				vswitch.Portgroup = append(vswitch.Portgroup[:i], vswitch.Portgroup[i+1:]...)
+				break
+			}
+		}
+
+		f := Map.getEntityParent(vswitch, "Folder").(*Folder)
+		f.removeChild(s.Reference())
+
+		for i, name := range vswitch.Summary.PortgroupName {
+			if name == s.Name {
+				vswitch.Summary.PortgroupName = append(vswitch.Summary.PortgroupName[:i],
+					vswitch.Summary.PortgroupName[i+1:]...)
+			}
+		}
+
+		return nil, nil
+	})
+
+	task.Run()
+
+	return &methods.Destroy_TaskBody{
+		Res: &types.Destroy_TaskResponse{
+			Returnval: task.Self,
+		},
+	}
+
+}

simulator/portgroup_test.go
@@ -1,0 +1,93 @@
+/*
+Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package simulator
+
+import (
+	"context"
+	"testing"
+
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/types"
+)
+
+func TestReconfigurePortgroup(t *testing.T) {
+	ctx := context.Background()
+
+	m := VPX()
+
+	err := m.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer m.Remove()
+
+	c := m.Service.client
+
+	dvs := object.NewDistributedVirtualSwitch(c,
+		Map.Any("DistributedVirtualSwitch").Reference())
+
+	spec := []types.DVPortgroupConfigSpec{
+		types.DVPortgroupConfigSpec{
+			Name:     "pg1",
+			NumPorts: 10,
+		},
+	}
+
+	task, err := dvs.AddPortgroup(ctx, spec)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = task.Wait(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pg := object.NewDistributedVirtualPortgroup(c,
+		Map.Any("DistributedVirtualPortgroup").Reference())
+	pgspec := types.DVPortgroupConfigSpec{
+		NumPorts: 5,
+		Name:     "pg1",
+	}
+
+	task, err = pg.Reconfigure(ctx, pgspec)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = task.Wait(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pge := Map.Get(pg.Reference()).(*DistributedVirtualPortgroup)
+	if pge.Config.Name != "pg1" || pge.Config.NumPorts != 5 {
+		t.Fatalf("expect pg.Name==pg1 && pg.Config.NumPort==5; got %s,%d",
+			pge.Config.Name, pge.Config.NumPorts)
+	}
+
+	task, err = pg.Destroy(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = task.Wait(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+}

commit ce88b296d1ca9630d23115b08964d3c886778212

Fix dvs.portgroup.info filtering
This regression was introduced by 585cf5e , where the DistributedVirtualSwitch
FetchDVPorts wrapper was added to the object package. The new method did not
include the optional 'Criteria' param, so no filtering was applied to the
dvs.portgroup.info command.  The command would panic on uplink portgroups, which
are normally filtered out by default.  But the same panic would happen 0.15 if
the '-uplinkPort' flag is given.

This change also adds an uplink portgroup to simulator switches, which would
reproduce the issue.

The dvs.portgroup.info command now has support for using the '-json' and '-dump' flags.

Fixes #1059
Fixes #1060

govc/dvs/portgroup/info.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 
 	"github.com/vmware/govmomi/govc/cli"
 	"github.com/vmware/govmomi/govc/flags"
@@ -37,7 +38,7 @@ type info struct {
 	inside     bool
 	uplinkPort bool
 	vlanID     int
-	count      uint
+	count      int
 }
 
 func init() {
@@ -54,15 +55,7 @@ func (cmd *info) Register(ctx context.Context, f *flag.FlagSet) {
 	f.BoolVar(&cmd.inside, "inside", true, "Filter by port inside or outside status")
 	f.BoolVar(&cmd.uplinkPort, "uplinkPort", false, "Filter for uplink ports")
 	f.IntVar(&cmd.vlanID, "vlan", 0, "Filter by VLAN ID (0 = unfiltered)")
-	f.UintVar(&cmd.count, "count", 0, "Number of matches to return (0 = unlimited)")
-}
-
-func (cmd *info) Process(ctx context.Context) error {
-	if err := cmd.ClientFlag.Process(ctx); err != nil {
-		return err
-	}
-
-	return nil
+	f.IntVar(&cmd.count, "count", 0, "Number of matches to return (0 = unlimited)")
 }
 
 func (cmd *info) Usage() string {
@@ -74,7 +67,37 @@ func (cmd *info) Description() string {
 
 Examples:
   govc dvs.portgroup.info DSwitch
+  govc dvs.portgroup.info -pg InternalNetwork DSwitch
   govc find / -type DistributedVirtualSwitch | xargs -n1 govc dvs.portgroup.info`
+}
+
+type infoResult struct {
+	Port []types.DistributedVirtualPort
+	cmd  *info
+}
+
+func (r *infoResult) Write(w io.Writer) error {
+	for _, port := range r.Port {
+		var vlanID int32
+		setting := port.Config.Setting.(*types.VMwareDVSPortSetting)
+
+		switch vlan := setting.Vlan.(type) {
+		case *types.VmwareDistributedVirtualSwitchVlanIdSpec:
+			vlanID = vlan.VlanId
+		case *types.VmwareDistributedVirtualSwitchTrunkVlanSpec:
+		case *types.VmwareDistributedVirtualSwitchPvlanSpec:
+			vlanID = vlan.PvlanId
+		}
+
+		// Show port info if: VLAN ID is not defined, or VLAN ID matches requested VLAN
+		if r.cmd.vlanID == 0 || vlanID == int32(r.cmd.vlanID) {
+			fmt.Printf("PortgroupKey: %s\n", port.PortgroupKey)
+			fmt.Printf("DvsUuid:      %s\n", port.DvsUuid)
+			fmt.Printf("VlanId:       %d\n", vlanID)
+			fmt.Printf("PortKey:      %s\n\n", port.Key)
+		}
+	}
+	return nil
 }
 
 func (cmd *info) Run(ctx context.Context, f *flag.FlagSet) error {
@@ -131,34 +154,20 @@ func (cmd *info) Run(ctx context.Context, f *flag.FlagSet) error {
 		criteria.PortgroupKey = []string{dvp.Key}
 	}
 
-	res, err := dvs.FetchDVPorts(ctx)
+	res, err := dvs.FetchDVPorts(ctx, &criteria)
 	if err != nil {
 		return err
 	}
 
-	var returnedPorts uint
-
-	// Iterate over returned ports
-	for _, port := range res {
-		portConfigSetting := port.Config.Setting.(*types.VMwareDVSPortSetting)
-		portVlan := portConfigSetting.Vlan.(*types.VmwareDistributedVirtualSwitchVlanIdSpec)
-		portVlanID := portVlan.VlanId
-
-		// Show port info if: VLAN ID is not defined, or VLAN ID matches requested VLAN
-		if cmd.vlanID == 0 || portVlanID == int32(cmd.vlanID) {
-			returnedPorts++
-
-			fmt.Printf("PortgroupKey: %s\n", port.PortgroupKey)
-			fmt.Printf("DvsUuid:      %s\n", port.DvsUuid)
-			fmt.Printf("VlanId:       %d\n", portVlanID)
-			fmt.Printf("PortKey:      %s\n\n", port.Key)
-
-			// If we are limiting the count and have reached the count, then stop returning output
-			if cmd.count > 0 && returnedPorts == cmd.count {
-				break
-			}
-		}
+	// Truncate output to -count if specified
+	if cmd.count > 0 && cmd.count < len(res) {
+		res = res[:cmd.count]
 	}
 
-	return nil
+	info := infoResult{
+		cmd:  cmd,
+		Port: res,
+	}
+
+	return cmd.WriteResult(&info)
 }

govc/flags/version.go
@@ -21,7 +21,7 @@ import (
 	"strings"
 )
 
-const Version = "0.17.0"
+const Version = "0.17.1"
 
 type version []int
 

object/distributed_virtual_switch.go
@@ -66,9 +66,10 @@ func (s DistributedVirtualSwitch) AddPortgroup(ctx context.Context, spec []types
 	return NewTask(s.Client(), res.Returnval), nil
 }
 
-func (s DistributedVirtualSwitch) FetchDVPorts(ctx context.Context) ([]types.DistributedVirtualPort, error) {
+func (s DistributedVirtualSwitch) FetchDVPorts(ctx context.Context, criteria *types.DistributedVirtualSwitchPortCriteria) ([]types.DistributedVirtualPort, error) {
 	req := &types.FetchDVPorts{
-		This: s.Reference(),
+		This:     s.Reference(),
+		Criteria: criteria,
 	}
 
 	res, err := methods.FetchDVPorts(ctx, s.Client(), req)

simulator/dvs.go
@@ -63,6 +63,12 @@ func (s *DistributedVirtualSwitch) AddDVPortgroupTask(c *types.AddDVPortgroup_Ta
 				VmVnicNetworkResourcePoolKey: spec.VmVnicNetworkResourcePoolKey,
 			}
 
+			if pg.Config.DefaultPortConfig == nil {
+				pg.Config.DefaultPortConfig = &types.VMwareDVSPortSetting{
+					Vlan: new(types.VmwareDistributedVirtualSwitchVlanIdSpec),
+				}
+			}
+
 			pg.PortKeys = []string{}
 
 			s.Portgroup = append(s.Portgroup, pg.Self)
@@ -113,10 +119,13 @@ func (s *DistributedVirtualSwitch) ReconfigureDvsTask(req *types.ReconfigureDvs_
 					Map.AddReference(pg, &pg.Host, member.Host)
 				}
 			case types.ConfigSpecOperationRemove:
-				if pg := FindReference(host.Network, s.Portgroup...); pg != nil {
-					return nil, &types.ResourceInUse{
-						Type: pg.Type,
-						Name: pg.Value,
+				for _, ref := range host.Vm {
+					vm := Map.Get(ref).(*VirtualMachine)
+					if pg := FindReference(vm.Network, s.Portgroup...); pg != nil {
+						return nil, &types.ResourceInUse{
+							Type: pg.Type,
+							Name: pg.Value,
+						}
 					}
 				}
 
@@ -171,10 +180,6 @@ func (s *DistributedVirtualSwitch) dvPortgroups(_ *types.DistributedVirtualSwitc
 				Setting: pg.Config.DefaultPortConfig,
 			},
 		})
-
-		if pg.PortKeys == nil {
-			continue
-		}
 
 		for _, key := range pg.PortKeys {
 			res = append(res, types.DistributedVirtualPort{

simulator/folder.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"math/rand"
 	"path"
+	"strings"
 
 	"github.com/google/uuid"
 
@@ -513,6 +514,17 @@ func (f *Folder) CreateDVSTask(req *types.CreateDVS_Task) soap.HasFault {
 				ForwardingClass: "etherswitch",
 			}
 		}
+
+		dvs.AddDVPortgroupTask(&types.AddDVPortgroup_Task{
+			Spec: []types.DVPortgroupConfigSpec{{
+				Name: dvs.Name + "-DVUplinks" + strings.TrimPrefix(dvs.Self.Value, "dvs"),
+				DefaultPortConfig: &types.VMwareDVSPortSetting{
+					Vlan: &types.VmwareDistributedVirtualSwitchTrunkVlanSpec{
+						VlanId: []types.NumericRange{{Start: 0, End: 4094}},
+					},
+				},
+			}},
+		})
 
 		return dvs.Reference(), nil
 	})

simulator/registry.go
@@ -40,6 +40,7 @@ var refValueMap = map[string]string{
 	"VirtualMachine":                 "vm",
 	"VirtualMachineSnapshot":         "snapshot",
 	"VmwareDistributedVirtualSwitch": "dvs",
+	"DistributedVirtualSwitch":       "dvs",
 }
 
 // Map is the default Registry instance.

govc/test/network.bats
@@ -176,6 +176,12 @@ load test_helper
   info=$(govc dvs.portgroup.info "$id" | grep VlanId: | uniq | grep 3123)
   [ -n "$info" ]
 
+  info=$(govc dvs.portgroup.info -json "$id" | jq  '.Port[].Config.Setting.Vlan | select(.VlanId == 3123)')
+  [ -n "$info" ]
+
+  info=$(govc dvs.portgroup.info -json "$id" | jq  '.Port[].Config.Setting.Vlan | select(.VlanId == 7777)')
+  [ -z "$info" ]
+
   run govc object.destroy "network/${id}-ExternalNetwork" "network/${id}-InternalNetwork" "network/${id}"
   assert_success
 }

govc/test/object.bats
@@ -158,7 +158,8 @@ load test_helper
   assert_matches dvportgroup-
 
   run govc object.collect -s -type DistributedVirtualPortgroup / config.name
-  assert_success DC0_DVPG0
+  assert_matches DC0_DVPG0
+  assert_matches DVS0-DVUplinks-
 
   run govc object.collect -s -type DistributedVirtualPortgroup / effectiveRole
   assert_number
@@ -292,7 +293,7 @@ load test_helper
   assert_number
 
   run govc object.collect -s -type Network / summary.accessible
-  assert_success "$(printf "true\ntrue")"
+  assert_success "$(printf "true\ntrue\ntrue")"
 
   run govc object.collect -s -type Network / summary.ipPoolName
   assert_success ""

simulator/dvs_test.go
@@ -29,7 +29,6 @@ import (
 
 func TestDVS(t *testing.T) {
 	m := VPX()
-	m.Portgroup = 0 // disabled DVS creation
 
 	defer m.Remove()
 
@@ -46,10 +45,11 @@ func TestDVS(t *testing.T) {
 	finder.SetDatacenter(dc[0])
 	folders, _ := dc[0].Folders(ctx)
 	hosts, _ := finder.HostSystemList(ctx, "*/*")
+	dvs0 := object.NewDistributedVirtualSwitch(c, Map.Any("DistributedVirtualSwitch").Reference())
 
 	var spec types.DVSCreateSpec
 	spec.ConfigSpec = &types.VMwareDVSConfigSpec{}
-	spec.ConfigSpec.GetDVSConfigSpec().Name = "DVS0"
+	spec.ConfigSpec.GetDVSConfigSpec().Name = "DVS1"
 
 	dtask, err := folders.NetworkFolder.CreateDVS(ctx, spec)
 	if err != nil {
@@ -82,16 +82,21 @@ func TestDVS(t *testing.T) {
 		{types.ConfigSpecOperationRemove, "", nil},                            // Remove == OK
 		{types.ConfigSpecOperationAdd, "", nil},                               // Add == OK
 		{types.ConfigSpecOperationAdd, "DVPG0", nil},                          // Add PG == OK
-		{types.ConfigSpecOperationRemove, "", &types.ResourceInUse{}},         // Remove == fail (ResourceInUse)
+		{types.ConfigSpecOperationRemove, "", &types.ResourceInUse{}},         // Remove dvs0 == fail (ResourceInUse)
+		{types.ConfigSpecOperationRemove, "", nil},                            // Remove dvs1 == OK (no VMs attached)
 		{types.ConfigSpecOperationRemove, "", &types.ManagedObjectNotFound{}}, // Remove == fail (ManagedObjectNotFound)
 	}
 
 	for x, test := range tests {
+		dswitch := dvs
+
 		switch test.err.(type) {
 		case *types.ManagedObjectNotFound:
 			for i := range config.Host {
 				config.Host[i].Host.Value = "enoent"
 			}
+		case *types.ResourceInUse:
+			dswitch = dvs0
 		}
 
 		if test.pg == "" {
@@ -99,11 +104,11 @@ func TestDVS(t *testing.T) {
 				config.Host[i].Operation = string(test.op)
 			}
 
-			dtask, err = dvs.Reconfigure(ctx, config)
+			dtask, err = dswitch.Reconfigure(ctx, config)
 		} else {
 			switch test.op {
 			case types.ConfigSpecOperationAdd:
-				dtask, err = dvs.AddPortgroup(ctx, []types.DVPortgroupConfigSpec{{Name: test.pg}})
+				dtask, err = dswitch.AddPortgroup(ctx, []types.DVPortgroupConfigSpec{{Name: test.pg}})
 			}
 		}
 
@@ -115,7 +120,7 @@ func TestDVS(t *testing.T) {
 
 		if test.err == nil {
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("%d: %s", x, err)
 			}
 			continue
 		}
@@ -129,12 +134,12 @@ func TestDVS(t *testing.T) {
 		}
 	}
 
-	ports, err := dvs.FetchDVPorts(ctx)
+	ports, err := dvs.FetchDVPorts(ctx, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(ports) != 1 {
-		t.Fatalf("expected 1 ports in DVPorts; got %d", len(ports))
+	if len(ports) != 2 {
+		t.Fatalf("expected 2 ports in DVPorts; got %d", len(ports))
 	}
 
 	dtask, err = dvs.Destroy(ctx)

simulator/finder_test.go
@@ -67,8 +67,8 @@ func TestFinderVPX(t *testing.T) {
 		{"ManagedObjectList", "/DC0", 1},
 		{"ManagedObjectList", "/F[01]", 2},
 		{"ManagedObjectListChildren", "/*", m.Datacenter + 3},
-		{"ManagedObjectListChildren", "/*/*", 18},
-		{"ManagedObjectListChildren", "/*/*/*", 24},
+		{"ManagedObjectListChildren", "/*/*", 19},
+		{"ManagedObjectListChildren", "/*/*/*", 25},
 		{"FolderList", "/*", m.Folder},
 		{"DatacenterList", "/F0/*", 1},
 		{"DatacenterList", "/DC0", 1},
@@ -91,13 +91,13 @@ func TestFinderVPX(t *testing.T) {
 		{"ClusterComputeResourceList", "/DC0/host/*", m.Cluster},
 		{"HostSystemList", "/DC0/host/*", m.Host + m.ClusterHost},
 		{"HostSystemList", "/F0/DC1/host/F0/*", m.Host + m.ClusterHost},
-		{"HostSystemList", "DC1_H0", 1},             // find . -type HostSystem -name DC1_H0
-		{"ComputeResourceList", "DC1_H0", 1},        // find . -type ComputeResource -name DC1_H0
-		{"ClusterComputeResourceList", "DC1_C0", 1}, // find . -type ClusterComputeResource -name DC1_H0
-		{"NetworkList", "/DC0/network/*", 2 + m.Portgroup},
+		{"HostSystemList", "DC1_H0", 1},                    // find . -type HostSystem -name DC1_H0
+		{"ComputeResourceList", "DC1_H0", 1},               // find . -type ComputeResource -name DC1_H0
+		{"ClusterComputeResourceList", "DC1_C0", 1},        // find . -type ClusterComputeResource -name DC1_H0
+		{"NetworkList", "/DC0/network/*", 3 + m.Portgroup}, // VM Network + DSwitch + DSwitch-Uplinks + m.Portgroup
 		{"NetworkList", "./*", 1},
 		{"NetworkList", "/F0/DC1/network/VM Network", 1},
-		{"NetworkList", "/F0/DC1/network/F0/*", 1 + m.Portgroup},
+		{"NetworkList", "/F0/DC1/network/F0/*", 2 + m.Portgroup},
 		{"NetworkList", "./F0/DC1_DVPG0", 1},
 		{"NetworkList", "./F0/DC1_DVPG0", 1},
 		{"NetworkList", "DC1_DVPG0", 1}, // find . -type Network -name DC1_DVPG0

simulator/model_test.go
@@ -31,6 +31,10 @@ func compareModel(t *testing.T, m *Model) {
 	pools := (m.Pool * m.Cluster * m.Datacenter) + (m.Host+m.Cluster)*m.Datacenter
 	// root folder + Datacenter folders {host,vm,datastore,network} + top-level folders
 	folders := 1 + (4 * m.Datacenter) + (5 * m.Folder)
+	pgs := m.Portgroup
+	if pgs > 0 {
+		pgs++ // uplinks
+	}
 
 	tests := []struct {
 		expect int
@@ -39,7 +43,7 @@ func compareModel(t *testing.T, m *Model) {
 	}{
 		{m.Datacenter, count.Datacenter, "Datacenter"},
 		{m.Cluster * m.Datacenter, count.Cluster, "Cluster"},
-		{m.Portgroup * m.Datacenter, count.Portgroup, "Portgroup"},
+		{pgs * m.Datacenter, count.Portgroup, "Portgroup"},
 		{m.Datastore * m.Datacenter, count.Datastore, "Datastore"},
 		{hosts, count.Host, "Host"},
 		{vms, count.Machine, "VirtualMachine"},