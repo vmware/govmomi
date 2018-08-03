8be5207c6836fc0888d8edd43f26c46370f2d1ab
Thibaut Ackermann <thibaut.ackermann@alcatel-lucent.com>
2018-08-03 10:07:26 +0200

    Add test for WaitOption.MaxWaitSeconds == 0 behaviour in simulator

--- simulator/property_collector_test.go
+++ simulator/property_collector_test.go
@@ -22,6 +22,7 @@ import (
 	"reflect"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
@@ -448,6 +449,115 @@ func TestIncrementalWaitForUpdates(t *testing.T) {
 	_, _ = vm.PowerOff(ctx)
 	_, _ = vm.Destroy(ctx)
 	wg.Wait() // wait for Delete to be reported
+}
+
+func TestWaitForUpdatesOneUpdateCalculation(t *testing.T) {
+	/*
+	 * In this test, we use WaitForUpdatesEx in non-blocking way
+	 * by setting the MaxWaitSeconds to 0.
+	 * We filter on 'runtime.powerState'
+	 * Once we get the first 'enter' update, we change the
+	 * power state of the VM to generate a 'modify' update.
+	 * Once we get the modify, we can stop.
+	 */
+	ctx := context.Background()
+
+	m := VPX()
+	defer m.Remove()
+
+	err := m.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s := m.Service.NewServer()
+	defer s.Close()
+
+	c, err := govmomi.NewClient(ctx, s.URL, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wait := make(chan bool)
+	pc := property.DefaultCollector(c.Client)
+	obj := Map.Any("VirtualMachine").(*VirtualMachine)
+	ref := obj.Reference()
+	vm := object.NewVirtualMachine(c.Client, ref)
+	filter := new(property.WaitFilter).Add(ref, ref.Type, []string{"runtime.powerState"})
+	// WaitOptions.maxWaitSeconds:
+	// A value of 0 causes WaitForUpdatesEx to do one update calculation and return any results.
+	filter.Options = &types.WaitOptions{
+		MaxWaitSeconds: types.NewInt32(0),
+	}
+	// toggle power state to generate updates
+	state := map[types.VirtualMachinePowerState]func(context.Context) (*object.Task, error){
+		types.VirtualMachinePowerStatePoweredOff: vm.PowerOn,
+		types.VirtualMachinePowerStatePoweredOn:  vm.PowerOff,
+	}
+
+	err = pc.CreateFilter(ctx, filter.CreateFilter)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := types.WaitForUpdatesEx{
+		This:    pc.Reference(),
+		Options: filter.Options,
+	}
+
+	go func() {
+		for {
+			res, err := methods.WaitForUpdatesEx(ctx, c.Client, &req)
+			if err != nil {
+				if ctx.Err() == context.Canceled {
+					werr := pc.CancelWaitForUpdates(context.Background())
+					t.Error(werr)
+					return
+				}
+				t.Error(err)
+				return
+			}
+
+			set := res.Returnval
+			if set == nil {
+				// Retry if the result came back empty
+				// Thats a normal case when MaxWaitSeconds is set to 0.
+				// It means we have no updates for now
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+
+			req.Version = set.Version
+
+			for _, fs := range set.FilterSet {
+				// We expect the enter of VM first
+				if fs.ObjectSet[0].Kind == types.ObjectUpdateKindEnter {
+					wait<-true
+					// Keep going
+					continue
+				}
+
+				// We also expect a modify due to the power state change
+				if fs.ObjectSet[0].Kind == types.ObjectUpdateKindModify {
+					wait<-true
+					// Now we can return to stop the routine
+					return
+				}
+			}
+		}
+	}()
+
+	// wait for the enter update.
+	<-wait
+
+	// Now change the VM power state, to generate a modify update
+	_, err = state[obj.Runtime.PowerState](ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// wait for the modify update.
+	<-wait
 }
 
 func TestPropertyCollectorWithUnsetValues(t *testing.T) {
