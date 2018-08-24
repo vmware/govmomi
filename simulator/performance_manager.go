d9019993cb69d219eac6281eccc8951a24346099
prydin <prydin@vmware.com>
2018-08-24 15:48:44 -0400

    Initial implementation of QueryPerf

--- simulator/performance_manager.go
+++ simulator/performance_manager.go
@@ -18,6 +18,7 @@ package simulator
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/vmware/govmomi/object"
@@ -64,6 +65,8 @@ func NewPerformanceManager(ref types.ManagedObjectReference) object.Reference {
 		m.hostMetrics = vpx.HostMetrics[:]
 		m.vmMetrics = vpx.VmMetrics[:]
 		m.rpMetrics = vpx.ResourcePoolMetrics[:]
+		m.clusterMetrics = vpx.ClusterMetrics[:]
+		m.datastoreMetrics = vpx.DatastoreMetrics[:]
 	}
 	m.perfCounterIndex = make(map[int32]types.PerfCounterInfo, len(m.PerfCounter))
 	for _, p := range m.PerfCounter {
@@ -134,29 +137,80 @@ func (p *PerformanceManager) buildAvailablePerfMetricsQueryResponse(ids []types.
 	return r
 }
 
+func (p *PerformanceManager) queryAvailablePerfMetric(entity types.ManagedObjectReference, interval int32) *types.QueryAvailablePerfMetricResponse {
+	var res *types.QueryAvailablePerfMetricResponse
+	switch entity.Type {
+	case "VirtualMachine":
+		vm := Map.Get(entity).(*VirtualMachine)
+		return p.buildAvailablePerfMetricsQueryResponse(p.vmMetrics, int(vm.Summary.Config.NumCpu), vm.Datastore[0].Value)
+	case "HostSystem":
+		host := Map.Get(entity).(*HostSystem)
+		return p.buildAvailablePerfMetricsQueryResponse(p.hostMetrics, int(host.Hardware.CpuInfo.NumCpuThreads), host.Datastore[0].Value)
+	case "ResourcePool":
+		return p.buildAvailablePerfMetricsQueryResponse(p.rpMetrics, 0, "")
+	case "ClusterComputeResource":
+		if interval != 20 {
+			res = p.buildAvailablePerfMetricsQueryResponse(p.clusterMetrics, 0, "")
+		}
+	case "Datastore":
+		if interval != 20 {
+			return p.buildAvailablePerfMetricsQueryResponse(p.datastoreMetrics, 0, "")
+		}
+	}
+
+	// Don't know how to handle this. Return empty response.
+	return new(types.QueryAvailablePerfMetricResponse)
+}
+
 func (p *PerformanceManager) QueryAvailablePerfMetric(ctx *Context, req *types.QueryAvailablePerfMetric) soap.HasFault {
 	body := new(methods.QueryAvailablePerfMetricBody)
 	body.Req = req
-	body.Res = new(types.QueryAvailablePerfMetricResponse)
+	body.Res = p.queryAvailablePerfMetric(req.Entity, req.IntervalId)
 
-	switch req.Entity.Type {
-	case "VirtualMachine":
-		vm := Map.Get(req.Entity).(*VirtualMachine)
-		body.Res = p.buildAvailablePerfMetricsQueryResponse(p.vmMetrics, int(vm.Summary.Config.NumCpu), vm.Datastore[0].Value)
-	case "HostSystem":
-		host := Map.Get(req.Entity).(*HostSystem)
-		body.Res = p.buildAvailablePerfMetricsQueryResponse(p.hostMetrics, int(host.Hardware.CpuInfo.NumCpuThreads), host.Datastore[0].Value)
-	case "ResourcePool":
-		body.Res = p.buildAvailablePerfMetricsQueryResponse(p.rpMetrics, 0, "")
-	case "ClusterComputeResource":
-		if req.IntervalId != 20 {
-			body.Res = p.buildAvailablePerfMetricsQueryResponse(p.clusterMetrics, 0, "")
-		}
-	case "Datastore":
-		if req.IntervalId != 20 {
-			body.Res = p.buildAvailablePerfMetricsQueryResponse(p.datastoreMetrics, 0, "")
-		}
-	}
 	spew.Dump(body)
 	return body
+}
+
+func (p *PerformanceManager) QueryPerf(ctx *Context, req *types.QueryPerf) soap.HasFault {
+	body := new(methods.QueryPerfBody)
+	body.Req = req
+	body.Res = new(types.QueryPerfResponse)
+	body.Res.Returnval = make([]types.BasePerfEntityMetricBase, len(req.QuerySpec))
+
+	for i, qs := range req.QuerySpec {
+		metrics := new(types.PerfEntityMetric)
+		metrics.Entity = qs.Entity
+
+		// Generate metric series. Divide into n buckets of interval seconds
+		interval := qs.IntervalId
+		if interval == -1 || interval == 0 {
+			interval = 20 // TODO: Determine from entity type
+		}
+		n := int32(qs.EndTime.Sub(*qs.StartTime).Seconds()) / interval
+		if n > qs.MaxSample {
+			n = qs.MaxSample
+		}
+
+		// Loop through each interval "tick"
+		metrics.SampleInfo = make([]types.PerfSampleInfo, n)
+		metrics.Value = make([]types.BasePerfMetricSeries, n)
+		ts := *qs.StartTime
+		for tick := int32(0); tick < n; tick++ {
+
+			// Create sample metadata
+			metrics.SampleInfo[tick] = types.PerfSampleInfo{Timestamp: ts, Interval: interval}
+
+			// Create list of metrics for this tick
+			series := &types.PerfMetricIntSeries{Value: make([]int64, len(qs.MetricId))}
+			for j, id := range qs.MetricId {
+				series.Value[j] = int64(0)
+			}
+			metrics.Value[tick] = series
+
+			// Advance to next interval tick
+			ts := ts.Add(time.Duration(interval) * time.Second)
+
+		}
+		body.Res.Returnval[i] = metrics
+	}
 }
