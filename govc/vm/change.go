d2ab2782eebc09545909e317d9a5ea528d966715
Doug MacEachern <dougm@vmware.com>
2019-04-04 11:06:28 -0700

    govc: add vm.change vpmc-enabled flag
    Fixes #1422

--- govc/vm/change.go
+++ govc/vm/change.go
@@ -98,6 +98,7 @@ func (cmd *change) Register(ctx context.Context, f *flag.FlagSet) {
 	f.Var(flags.NewOptionalBool(&cmd.NestedHVEnabled), "nested-hv-enabled", "Enable nested hardware-assisted virtualization")
 	cmd.Tools = &types.ToolsConfigInfo{}
 	f.Var(flags.NewOptionalBool(&cmd.Tools.SyncTimeWithHost), "sync-time-with-host", "Enable SyncTimeWithHost")
+	f.Var(flags.NewOptionalBool(&cmd.VPMCEnabled), "vpmc-enabled", "Enable CPU performance counters")
 }
 
 func (cmd *change) Description() string {
