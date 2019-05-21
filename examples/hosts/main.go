a4f58ac69fadb1f9c8d9ea2f484e4c20e351046f
Amanda Hager Lopes de Andrade Katz <amanda.katz@serpro.gov.br>
2019-05-21 12:03:31 -0300

    Update examples to use examples.Run method

--- examples/hosts/main.go
+++ examples/hosts/main.go
@@ -24,63 +24,58 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"text/tabwriter"
 
 	"github.com/vmware/govmomi/examples"
 	"github.com/vmware/govmomi/units"
 	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/mo"
 )
 
 func main() {
-	ctx := context.Background()
+	examples.Run(func(ctx context.Context, c *vim25.Client) error {
 
-	// Connect and login to ESX or vCenter
-	c, err := examples.NewClient(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+		// Create a view of HostSystem objects
+		m := view.NewManager(c)
 
-	defer c.Logout(ctx)
+		v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"HostSystem"}, true)
+		if err != nil {
+			return err
+		}
 
-	// Create a view of HostSystem objects
-	m := view.NewManager(c.Client)
+		defer v.Destroy(ctx)
 
-	v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"HostSystem"}, true)
-	if err != nil {
-		log.Fatal(err)
-	}
+		// Retrieve summary property for all hosts
+		// Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.HostSystem.html
+		var hss []mo.HostSystem
+		err = v.Retrieve(ctx, []string{"HostSystem"}, []string{"summary"}, &hss)
+		if err != nil {
+			return err
+		}
 
-	defer v.Destroy(ctx)
+		// Print summary per host (see also: govc/host/info.go)
 
-	// Retrieve summary property for all hosts
-	// Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.HostSystem.html
-	var hss []mo.HostSystem
-	err = v.Retrieve(ctx, []string{"HostSystem"}, []string{"summary"}, &hss)
-	if err != nil {
-		log.Fatal(err)
-	}
+		tw := tabwriter.NewWriter(os.Stdout, 2, 0, 2, ' ', 0)
+		fmt.Fprintf(tw, "Name:\tUsed CPU:\tTotal CPU:\tFree CPU:\tUsed Memory:\tTotal Memory:\tFree Memory:\t\n")
 
-	// Print summary per host (see also: govc/host/info.go)
+		for _, hs := range hss {
+			totalCPU := int64(hs.Summary.Hardware.CpuMhz) * int64(hs.Summary.Hardware.NumCpuCores)
+			freeCPU := int64(totalCPU) - int64(hs.Summary.QuickStats.OverallCpuUsage)
+			freeMemory := int64(hs.Summary.Hardware.MemorySize) - (int64(hs.Summary.QuickStats.OverallMemoryUsage) * 1024 * 1024)
+			fmt.Fprintf(tw, "%s\t", hs.Summary.Config.Name)
+			fmt.Fprintf(tw, "%d\t", hs.Summary.QuickStats.OverallCpuUsage)
+			fmt.Fprintf(tw, "%d\t", totalCPU)
+			fmt.Fprintf(tw, "%d\t", freeCPU)
+			fmt.Fprintf(tw, "%s\t", (units.ByteSize(hs.Summary.QuickStats.OverallMemoryUsage))*1024*1024)
+			fmt.Fprintf(tw, "%s\t", units.ByteSize(hs.Summary.Hardware.MemorySize))
+			fmt.Fprintf(tw, "%d\t", freeMemory)
+			fmt.Fprintf(tw, "\n")
+		}
 
-	tw := tabwriter.NewWriter(os.Stdout, 2, 0, 2, ' ', 0)
-	fmt.Fprintf(tw, "Name:\tUsed CPU:\tTotal CPU:\tFree CPU:\tUsed Memory:\tTotal Memory:\tFree Memory:\t\n")
+		_ = tw.Flush()
 
-	for _, hs := range hss {
-		totalCPU := int64(hs.Summary.Hardware.CpuMhz) * int64(hs.Summary.Hardware.NumCpuCores)
-		freeCPU := int64(totalCPU) - int64(hs.Summary.QuickStats.OverallCpuUsage)
-		freeMemory := int64(hs.Summary.Hardware.MemorySize) - (int64(hs.Summary.QuickStats.OverallMemoryUsage) * 1024 * 1024)
-		fmt.Fprintf(tw, "%s\t", hs.Summary.Config.Name)
-		fmt.Fprintf(tw, "%d\t", hs.Summary.QuickStats.OverallCpuUsage)
-		fmt.Fprintf(tw, "%d\t", totalCPU)
-		fmt.Fprintf(tw, "%d\t", freeCPU)
-		fmt.Fprintf(tw, "%s\t", (units.ByteSize(hs.Summary.QuickStats.OverallMemoryUsage))*1024*1024)
-		fmt.Fprintf(tw, "%s\t", units.ByteSize(hs.Summary.Hardware.MemorySize))
-		fmt.Fprintf(tw, "%d\t", freeMemory)
-		fmt.Fprintf(tw, "\n")
-	}
-
-	_ = tw.Flush()
+		return nil
+	})
 }
