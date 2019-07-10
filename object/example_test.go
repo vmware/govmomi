9495f0d8b2d64cc7bc98742ebfb3f1e8bb7cb0d7
Doug MacEachern <dougm@vmware.com>
2019-07-10 16:08:15 -0700

    examples: add CustomFieldManager.Set
    Fixes #1513

--- object/example_test.go
+++ object/example_test.go
@@ -19,11 +19,15 @@ package object_test
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/property"
 	"github.com/vmware/govmomi/simulator"
+	"github.com/vmware/govmomi/view"
 	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/vim25/types"
 )
 
@@ -123,4 +127,57 @@ func ExampleCommon_Destroy() {
 		return nil
 	}, model)
 	// Output: destroyed /DC0/datastore/LocalDS_1
+}
+
+func ExampleCustomFieldsManager_Set() {
+	simulator.Example(func(ctx context.Context, c *vim25.Client) error {
+		m, err := object.GetCustomFieldsManager(c)
+		if err != nil {
+			return err
+		}
+
+		any := []string{"ManagedEntity"}
+		field, err := m.Add(ctx, "backup", any[0], nil, nil) // adds the custom field "backup" to all types
+		if err != nil {
+			return err
+		}
+
+		v, err := view.NewManager(c).CreateContainerView(ctx, c.ServiceContent.RootFolder, any, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		all, err := v.Find(ctx, any, nil) // gives us the count of all objects in the inventory
+		if err != nil {
+			return err
+		}
+
+		refs, err := v.Find(ctx, []string{"VirtualMachine", "Datastore"}, nil)
+		if err != nil {
+			return err
+		}
+
+		for _, ref := range refs {
+			err = m.Set(ctx, ref, field.Key, "true") // sets the custom value "backup=true" on specific types
+			if err != nil {
+				return err
+			}
+		}
+
+		// filter used to find objects with "backup=true"
+		filter := property.Filter{"customValue": &types.CustomFieldStringValue{
+			CustomFieldValue: types.CustomFieldValue{Key: field.Key},
+			Value:            "true",
+		}}
+
+		var objs []mo.ManagedEntity
+		err = v.RetrieveWithFilter(ctx, any, []string{"name", "customValue"}, &objs, filter)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("backup %d of %d objects", len(objs), len(all))
+		return v.Destroy(ctx)
+	})
+	// Output: backup 5 of 22 objects
 }
