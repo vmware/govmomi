30fc22253ef489ce6e549e1b3adbfd5ed0868bb3
François Rigault <rigault.francois@gmail.com>
2019-10-03 21:03:21 +0200

    Report errors when cdrom.insert fails
    For errors such as datastore not specified, cdrom.insert should report the error.

--- govc/device/cdrom/insert.go
+++ govc/device/cdrom/insert.go
@@ -89,7 +89,7 @@ func (cmd *insert) Run(ctx context.Context, f *flag.FlagSet) error {
 
 	iso, err := cmd.DatastorePath(f.Arg(0))
 	if err != nil {
-		return nil
+		return err
 	}
 
 	return vm.EditDevice(ctx, devices.InsertIso(c, iso))
