63dd5e9bc326197ae10adc305d6443f0af221fde
Antony Saba <awsaba@gmail.com>
2021-09-15 15:57:55 -0600

    Local default registry references for simulator/virtual_machine_fault_test.go

--- simulator/virtual_machine_fault_test.go
+++ simulator/virtual_machine_fault_test.go
@@ -37,11 +37,12 @@ func TestSwitchMembers(t *testing.T) {
 		vm, _ := finder.VirtualMachine(ctx, "DC0_C0_RP0_VM0")
 		host, _ := vm.HostSystem(ctx)
 
+		vimMap := simulator.Map()
 		// The proper way to remove a host from is with dvs.Reconfigure + types.ConfigSpecOperationRemove,
 		// but that would fail here with ResourceInUse. And creating a new DVS + PG is cumbersome,
 		// so just force the removal of host from the DVS + PG, which has the same effect on vm.AddDevice().
-		dswitch := simulator.Map().Get(dvs.Reference()).(*simulator.DistributedVirtualSwitch)
-		portgrp := simulator.Map().Get(pg.Reference()).(*simulator.DistributedVirtualPortgroup)
+		dswitch := vimMap.Get(dvs.Reference()).(*simulator.DistributedVirtualSwitch)
+		portgrp := vimMap.Get(pg.Reference()).(*simulator.DistributedVirtualPortgroup)
 		simulator.RemoveReference(&dswitch.Summary.HostMember, host.Reference())
 		simulator.RemoveReference(&portgrp.Host, host.Reference())
 
