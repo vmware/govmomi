642156dd6e5118c1ceecd396b885ac7a17d67d87
Adam Fowler <adam@adamfowler.org>
2022-07-20 12:56:42 +0100

    api: Adds vSphere 7.0u1-u3 support to namespace-management (Tanzu)
    Closes: #2860
    Signed-off-by: Adam Fowler <adam@adamfowler.org>

--- vapi/namespace/simulator/simulator.go
+++ vapi/namespace/simulator/simulator.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020 VMware, Inc. All Rights Reserved.
+Copyright (c) 2020-2022 VMware, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -104,8 +104,8 @@ func (h *Handler) clusters(w http.ResponseWriter, r *http.Request) {
 		for i, ref := range refs {
 			clusters[i] = namespace.ClusterSummary{
 				ID:               ref.Value,
-				ConfigStatus:     "RUNNING",
-				KubernetesStatus: "READY",
+				ConfigStatus:     &namespace.RunningConfigStatus,
+				KubernetesStatus: &namespace.ReadyKubernetesStatus,
 			}
 		}
 		vapi.StatusOK(w, clusters)
