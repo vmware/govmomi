76e99b0014c03b98b6e89eb7b2e83601ad88cc36
martin <martin@catai.org>
2022-08-01 13:08:55 +0200

    Boilerplate check requires a date range in the license.

--- toolbox/hgfs/hgfs_openbsd.go
+++ toolbox/hgfs/hgfs_openbsd.go
@@ -2,7 +2,7 @@
 // +build openbsd
 
 /*
-Copyright (c) 2022 VMware, Inc. All Rights Reserved.
+Copyright (c) 2022-2022 VMware, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
