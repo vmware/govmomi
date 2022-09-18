4a9b0e33db2b32824cccc9158fcd7f660c8f5ed1
Hrabur Stoyanov <hstoyanov@vmware.com>
2022-09-18 14:55:53 +0300

    move FindUsersInGroup to users.ls
    Closes: #2945

--- govc/sso/user/ls.go
+++ govc/sso/user/ls.go
@@ -35,6 +35,8 @@ type ls struct {
 	*flags.OutputFlag
 
 	solution bool
+	group    bool
+	search   string
 }
 
 func init() {
@@ -49,13 +51,16 @@ func (cmd *ls) Register(ctx context.Context, f *flag.FlagSet) {
 	cmd.OutputFlag.Register(ctx, f)
 
 	f.BoolVar(&cmd.solution, "s", false, "List solution users")
+	f.BoolVar(&cmd.group, "group", false, "List users in group")
+	f.StringVar(&cmd.search, "search", "", "Search users in group")
 }
 
 func (cmd *ls) Description() string {
 	return `List SSO users.
 
 Examples:
-  govc sso.user.ls -s`
+  govc sso.user.ls -s
+  govc sso.user.ls -group group-name`
 }
 
 func (cmd *ls) Process(ctx context.Context) error {
@@ -63,6 +68,20 @@ func (cmd *ls) Process(ctx context.Context) error {
 		return err
 	}
 	return cmd.OutputFlag.Process(ctx)
+}
+
+type userResult []types.AdminUser
+
+func (r userResult) Dump() interface{} {
+	return []types.AdminUser(r)
+}
+
+func (r userResult) Write(w io.Writer) error {
+	tw := tabwriter.NewWriter(w, 2, 0, 2, ' ', 0)
+	for _, info := range r {
+		fmt.Fprintf(tw, "%s\t%s\n", info.Id.Name, info.Description)
+	}
+	return tw.Flush()
 }
 
 type solutionResult []types.AdminSolutionUser
@@ -98,6 +117,9 @@ func (cmd *ls) Run(ctx context.Context, f *flag.FlagSet) error {
 
 	return sso.WithClient(ctx, cmd.ClientFlag, func(c *ssoadmin.Client) error {
 		if cmd.solution {
+			if f.NArg() != 0 {
+				return flag.ErrHelp
+			}
 			info, err := c.FindSolutionUsers(ctx, arg)
 			if err != nil {
 				return err
@@ -105,7 +127,17 @@ func (cmd *ls) Run(ctx context.Context, f *flag.FlagSet) error {
 
 			return cmd.WriteResult(solutionResult(info))
 		}
+		if cmd.group {
+			if f.NArg() == 0 {
+				return flag.ErrHelp
+			}
+			info, err := c.FindUsersInGroup(ctx, f.Arg(0), cmd.search)
+			if err != nil {
+				return err
+			}
 
+			return cmd.WriteResult(userResult(info))
+		}
 		info, err := c.FindPersonUsers(ctx, arg)
 		if err != nil {
 			return err
