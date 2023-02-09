3bf46949e6a13edb998fa76eabc19efba30ca7b5
Doug MacEachern <dougm@vmware.com>
2023-02-09 14:03:28 -0800

    examples: add session.Manager.LoginByToken
    Issue #3037

---
+++ session/example_test.go
@@ -0,0 +1,74 @@
+/*
+Copyright (c) 2023-2023 VMware, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package session_test
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	_ "github.com/vmware/govmomi/lookup/simulator"
+	"github.com/vmware/govmomi/session"
+	"github.com/vmware/govmomi/simulator"
+	"github.com/vmware/govmomi/sts"
+	_ "github.com/vmware/govmomi/sts/simulator"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/soap"
+)
+
+func ExampleManager_LoginByToken() {
+	simulator.Run(func(ctx context.Context, vc *vim25.Client) error {
+		c, err := sts.NewClient(ctx, vc)
+		if err != nil {
+			return err
+		}
+
+		// Issue a bearer token
+		req := sts.TokenRequest{
+			Userinfo: url.UserPassword("Administrator@VSPHERE.LOCAL", "password"),
+		}
+
+		signer, err := c.Issue(ctx, req)
+		if err != nil {
+			return err
+		}
+
+		// Create a new un-authenticated client and LoginByToken
+		vc2, err := vim25.NewClient(ctx, soap.NewClient(vc.URL(), true))
+		if err != nil {
+			return err
+		}
+
+		m := session.NewManager(vc2)
+		header := soap.Header{Security: signer}
+
+		err = m.LoginByToken(vc2.WithHeader(ctx, header))
+		if err != nil {
+			return err
+		}
+
+		session, err := m.UserSession(ctx)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(session.UserName)
+
+		return nil
+	})
+	// Output: Administrator@VSPHERE.LOCAL
+}
