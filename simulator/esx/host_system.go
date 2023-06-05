11356057dd92133cbb7573a060a94db2fb4483b1
Ueli Banholzer <ueli@whatwedo.ch>
2023-06-05 23:43:02 +0200

    vcsim: set StandbyMode default to "none"
    according Enum HostStandbyMode(vim.HostSystem.StandbyMode), StandByMode
    should not be empty

--- simulator/esx/host_system.go
+++ simulator/esx/host_system.go
@@ -53,7 +53,7 @@ var HostSystem = mo.HostSystem{
 		DynamicData:       types.DynamicData{},
 		ConnectionState:   "connected",
 		PowerState:        "poweredOn",
-		StandbyMode:       "",
+		StandbyMode:       "none",
 		InMaintenanceMode: false,
 		BootTime:          (*time.Time)(nil),
 		HealthSystemRuntime: &types.HealthSystemRuntime{
