5090e85640846c395dfe596f2615e5b2236f963a
Deyan Popov <deyan.popov@gmail.com>
2023-08-03 15:13:25 +0300

    vcsim: Fix RefreshDatastore to return a valid response
    Closes: #3198

--- simulator/datastore.go
+++ simulator/datastore.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+Copyright (c) 2017-2023 VMware, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -83,6 +83,7 @@ func (ds *Datastore) RefreshDatastore(*types.RefreshDatastore) soap.HasFault {
 
 	info.Timestamp = types.NewTime(time.Now())
 
+	r.Res = &types.RefreshDatastoreResponse{}
 	return r
 }
 
