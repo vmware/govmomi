3784b2f26633c979ded51354648d2375c4d15e74
jitinkumar94 <jitinkumar94@github.com>
2024-03-15 15:27:36 +0530

    api: Add Downloadendpoint field in the library item file struct
    Closes: #3386

--- vapi/library/library_file.go
+++ vapi/library/library_file.go
@@ -31,11 +31,12 @@ type Checksum struct {
 
 // File provides methods to get information on library item files.
 type File struct {
-	Cached   *bool     `json:"cached,omitempty"`
-	Checksum *Checksum `json:"checksum_info,omitempty"`
-	Name     string    `json:"name,omitempty"`
-	Size     *int64    `json:"size,omitempty"`
-	Version  string    `json:"version,omitempty"`
+	Cached           *bool     `json:"cached,omitempty"`
+	Checksum         *Checksum `json:"checksum_info,omitempty"`
+	Name             string    `json:"name,omitempty"`
+	Size             *int64    `json:"size,omitempty"`
+	Version          string    `json:"version,omitempty"`
+	DownloadEndpoint string    `json:"file_download_endpoint,omitempty"`
 }
 
 // ListLibraryItemFiles returns a list of all the files for a library item.
