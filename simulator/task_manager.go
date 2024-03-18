9c59e9c55da4d7bd7316a626404cc956da4f6c01
Doug MacEachern <dougm@vmware.com>
2024-03-17 22:02:55 -0700

    vcsim: add TaskManager.CreateTask support

--- simulator/task_manager.go
+++ simulator/task_manager.go
@@ -1,11 +1,11 @@
 /*
-Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+Copyright (c) 2017-2024 VMware, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,10 +18,13 @@ package simulator
 
 import (
 	"sync"
+	"time"
 
 	"github.com/vmware/govmomi/simulator/esx"
 	"github.com/vmware/govmomi/simulator/vpx"
+	"github.com/vmware/govmomi/vim25/methods"
 	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/soap"
 	"github.com/vmware/govmomi/vim25/types"
 )
 
@@ -62,3 +65,47 @@ func (m *TaskManager) PutObject(obj mo.Reference) {
 func (*TaskManager) RemoveObject(*Context, types.ManagedObjectReference) {}
 
 func (*TaskManager) UpdateObject(mo.Reference, []types.PropertyChange) {}
+
+func validTaskID(ctx *Context, taskID string) bool {
+	m := ctx.Map.ExtensionManager()
+
+	for _, x := range m.ExtensionList {
+		for _, task := range x.TaskList {
+			if task.TaskID == taskID {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (m *TaskManager) CreateTask(ctx *Context, req *types.CreateTask) soap.HasFault {
+	body := &methods.CreateTaskBody{}
+
+	if !validTaskID(ctx, req.TaskTypeId) {
+		body.Fault_ = Fault("", &types.InvalidArgument{
+			InvalidProperty: "taskType",
+		})
+		return body
+	}
+
+	task := &Task{}
+
+	task.Self = ctx.Map.newReference(task)
+	task.Info.Key = task.Self.Value
+	task.Info.Task = task.Self
+	task.Info.DescriptionId = req.TaskTypeId
+	task.Info.Cancelable = req.Cancelable
+	task.Info.Entity = &req.Obj
+	task.Info.EntityName = req.Obj.Value
+	task.Info.Reason = &types.TaskReasonUser{UserName: ctx.Session.UserName}
+	task.Info.QueueTime = time.Now()
+	task.Info.State = types.TaskInfoStateQueued
+
+	body.Res = &types.CreateTaskResponse{Returnval: task.Info}
+
+	go ctx.Map.Put(task)
+
+	return body
+}
