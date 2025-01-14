228a1024b6f038390b5c44f3ff4eacbb55116d57
Doug MacEachern <dougm@broadcom.com>
2025-01-14 15:30:36 -0800

    vcsim: fix lookup service deferred registration
    With this change, List() can be called withing having called RetrieveServiceContent()
    
    Signed-off-by: Doug MacEachern <dougm@broadcom.com>

--- lookup/simulator/registration_info.go
+++ lookup/simulator/registration_info.go
@@ -1,18 +1,6 @@
-/*
-Copyright (c) 2018-2023 VMware, Inc. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// © Broadcom. All Rights Reserved.
+// The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: Apache-2.0
 
 package simulator
 
@@ -31,10 +19,11 @@ var (
 
 // registrationInfo returns a ServiceRegistration populated with vcsim's OptionManager settings.
 // The complete list can be captured using: govc sso.service.ls -dump
-func registrationInfo() []types.LookupServiceRegistrationInfo {
-	vc := simulator.Map.Get(vim25.ServiceInstance).(*simulator.ServiceInstance)
-	setting := simulator.Map.OptionManager().Setting
-	sm := simulator.Map.SessionManager()
+func registrationInfo(ctx *simulator.Context) []types.LookupServiceRegistrationInfo {
+	vctx := ctx.For(vim25.Path)
+	vc := vctx.Map.Get(vim25.ServiceInstance).(*simulator.ServiceInstance)
+	setting := vctx.Map.OptionManager().Setting
+	sm := vctx.Map.SessionManager()
 	opts := make(map[string]string, len(setting))
 
 	for _, o := range setting {
