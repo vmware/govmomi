35b5564b45241692f3b3f1e3e7478dab25494fad
Doug MacEachern <dougm@broadcom.com>
2025-02-07 15:13:10 -0800

    govc: Rewrite kms.export URL to use the host we connected to vCenter with
    Signed-off-by: Doug MacEachern <dougm@broadcom.com>

--- cli/kms/export.go
+++ cli/kms/export.go
@@ -1,18 +1,6 @@
-/*
-Copyright (c) 2024-2024 VMware, Inc. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// © Broadcom. All Rights Reserved.
+// The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: Apache-2.0
 
 package kms
 
@@ -20,6 +8,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"net/url"
 
 	"github.com/vmware/govmomi/cli"
 	"github.com/vmware/govmomi/cli/flags"
@@ -79,6 +68,15 @@ func (cmd *export) Run(ctx context.Context, f *flag.FlagSet) error {
 	if export.Type != "LOCATION" {
 		return fmt.Errorf("unsupported export type: %s", export.Type)
 	}
+
+	// Rewrite URL to use the host we connected to vCenter with
+	u, err := url.Parse(export.Location.URL)
+	if err != nil {
+		return err
+	}
+
+	u.Host = rc.URL().Host
+	export.Location.URL = u.String()
 
 	req, err := m.KmsProviderExportRequest(ctx, export.Location)
 	if err != nil {
